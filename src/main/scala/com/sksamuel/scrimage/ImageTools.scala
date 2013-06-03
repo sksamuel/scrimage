48bc050c89ec63cf8c43a8986565413316fae454
sksamuel <sam@sksamuel.com>
2013-06-03 22:03:47 +0100

    added tools for resizing and fitting images

---
+++ src/main/scala/com/sksamuel/scrimage/ImageTools.scala
@@ -0,0 +1,93 @@
+package com.sksamuel.scrimage
+
+import java.awt.image.BufferedImage
+import org.apache.commons.io.FilenameUtils
+import java.net.URLConnection
+import java.awt.{Image, Color}
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+/** @author Stephen Samuel */
+object ImageTools {
+
+    val BG_COLOR = Color.WHITE
+    val SCALING_METHOD = java.awt.Image.SCALE_AREA_AVERAGING
+
+    // write out the image to bytes
+    def toBytes(image: BufferedImage, format: String) = {
+        require(format != null)
+        val output = new ByteArrayOutputStream
+        ImageIO.write(image, format, output)
+        output.toByteArray
+    }
+
+    def contentType(filename: String) = {
+        val ext = FilenameUtils.getExtension(filename)
+        ext match {
+            case "gif" => "image/gif"
+            case "jpg" => "image/jpg"
+            case "jpeg" => "image/jpeg"
+            case "png" => "image/png"
+            case _ => Option(URLConnection.guessContentTypeFromName(filename)).getOrElse("application/octet-stream")
+        }
+    }
+
+    /**
+     * Scales the given image to fit the target dimensions while keeping the current aspect ratio.
+     */
+    def fit(source: Image, size: (Int, Int)): BufferedImage = {
+        val sizePrime = normalizeSize(source, size)
+        val fitted = dimensionsToFit(sizePrime, (source.getWidth(null), source.getHeight(null)))
+        val scaled = source.getScaledInstance(fitted._1, fitted._2, SCALING_METHOD)
+        val offset = ((sizePrime._1 - fitted._1) / 2, (sizePrime._2 - fitted._2) / 2)
+        _draw(sizePrime, offset, scaled)
+    }
+
+    def normalizeSize(source: Image, size: (Int, Int)) = size match {
+        case (0, 0) => (0, 0)
+        case (0, h) => ((ratio(source) * h).toInt, h)
+        case (w, 0) => (w, (w.toDouble / ratio(source)).toInt)
+        case (w, h) => (w, h)
+    }
+
+    private def _draw(size: (Int, Int), offset: (Int, Int), image: Image) = {
+        val target = new BufferedImage(size._1, size._2, BufferedImage.TYPE_INT_RGB)
+        val g = target.createGraphics
+        g.setColor(BG_COLOR)
+        g.fillRect(0, 0, size._1, size._2)
+        g.drawImage(image, offset._1, offset._2, null)
+        target
+    }
+
+    /**
+     * Resizes the given image into the new target dimensions.
+     */
+    def resize(source: Image, size: (Int, Int)): BufferedImage = {
+        val sizePrime = normalizeSize(source, size)
+        val scaled = source.getScaledInstance(sizePrime._1, sizePrime._2, SCALING_METHOD)
+        _draw(sizePrime, (0, 0), scaled)
+    }
+
+    private[image] def ratio(source: Image): Double = ratio(source.getWidth(null), source.getHeight(null))
+    private[image] def ratio(width: Int, height: Int): Double = if (height == 0) 0 else width / height.toDouble
+
+    /**
+     * Returns width and height that allow the given source width, height to fit inside the target width, height
+     * without losing aspect ratio
+     */
+    def dimensionsToFit(target: (Int, Int), source: (Int, Int)): (Int, Int) = {
+
+        // if target width/height is zero then we have no preference for that, so set it to the original value,
+        // since it cannot be any larger
+        val maxWidth = if (target._1 == 0) source._1 else target._1
+        val maxHeight = if (target._2 == 0) source._2 else target._2
+
+        val wscale = maxWidth / source._1.toDouble
+        val hscale = maxHeight / source._2.toDouble
+
+        if (wscale < hscale)
+            ((source._1 * wscale).toInt, (source._2 * wscale).toInt)
+        else
+            ((source._1 * hscale).toInt, (source._2 * hscale).toInt)
+    }
+}
---
+++ src/test/scala/com/sksamuel/scrimage/ImageToolsTest.scala
@@ -0,0 +1,109 @@
+package com.sksamuel.scrimage
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import java.awt.image.BufferedImage
+
+/** @author Stephen Samuel */
+class ImageToolsTest extends FunSuite with BeforeAndAfter {
+
+    test("when writing out an image then the array is the right length") {
+        val image = new BufferedImage(100, 200, BufferedImage.TYPE_INT_BGR)
+        val actual = ImageTools.toBytes(image, "PNG")
+        assert(actual.length === 138)
+    }
+
+    test("when normalizing dimensions with width 0 then the aspect ratio is used") {
+        val image = new BufferedImage(100, 200, BufferedImage.TYPE_INT_RGB)
+        val size = ImageTools.normalizeSize(image, (0, 100))
+        assert(size === (50, 100))
+    }
+
+    test("when normalizing dimensions with height 0 then the aspect ratio is used") {
+        val image = new BufferedImage(300, 200, BufferedImage.TYPE_INT_RGB)
+        val size = ImageTools.normalizeSize(image, (150, 0))
+        assert(size === (150, 100))
+    }
+
+    test("when normalizing dimensions with width and height 0 then 0,0 is returned") {
+        val image = new BufferedImage(300, 200, BufferedImage.TYPE_INT_RGB)
+        val size = ImageTools.normalizeSize(image, (0, 0))
+        assert(size === (0, 0))
+    }
+
+    test("when given a vertical box then it is scaled to fit") {
+        val fitted = ImageTools.dimensionsToFit((500, 500), (500, 2000))
+        assert(fitted === (125, 500))
+    }
+
+    test("when given a horizontal box then it is scaled to fit") {
+        val fitted = ImageTools.dimensionsToFit((500, 500), (1000, 500))
+        assert(fitted === (500, 250))
+    }
+
+    test("when given the same dimensions the ratio returns 1") {
+        val ratio = ImageTools.ratio(5, 5)
+        assert(ratio === 1)
+    }
+
+    test("when given some dimensions the correct ratio is returned") {
+        val ratio = ImageTools.ratio(10, 5)
+        assert(ratio === 2)
+
+        val ratio2 = ImageTools.ratio(5, 20)
+        assert(ratio2 === 0.25)
+    }
+
+    test("when given a zero dimension the ratio returns zero") {
+        val ratio = ImageTools.ratio(5, 0)
+        assert(ratio === 0)
+
+        val ratio2 = ImageTools.ratio(0, 5)
+        assert(ratio2 === 0)
+    }
+
+    test("when given a box that is already the same size as the target then the same dimensions are returned") {
+        val fitted = ImageTools.dimensionsToFit((333, 333), (333, 333))
+        assert(fitted === (333, 333))
+    }
+
+    test("when given a .gif file then image/gif is detected for the content type") {
+        val contentType = ImageTools.contentType("coldplay.gif")
+        assert(contentType === "image/gif")
+    }
+
+    test("when given a .png file then image/png is detected for the content type") {
+        val contentType = ImageTools.contentType("coldplay.png")
+        assert(contentType === "image/png")
+    }
+
+    test("when given a .jpeg file then image/jpeg is detected for the content type") {
+        val contentType = ImageTools.contentType("coldplay.jpeg")
+        assert(contentType === "image/jpeg")
+    }
+
+    test("when given a .jpg file then image/jpeg is detected for the content type") {
+        val contentType = ImageTools.contentType("coldplay.jpg")
+        assert(contentType === "image/jpg")
+    }
+
+    test("when given a non image file then the content type is detected from the filename") {
+        assert(ImageTools.contentType("coldplay.pdf") === "application/pdf")
+        assert(ImageTools.contentType("coldplay.doc") === "application/octet-stream")
+        assert(ImageTools.contentType("coldplay.txt") === "text/plain")
+    }
+
+    test("when resizing an image then the resultant image is the expected size") {
+        val image = new BufferedImage(100, 200, BufferedImage.TYPE_INT_RGB)
+        val actual = ImageTools.resize(image, (50, 80))
+        assert(actual.getWidth === 50)
+        assert(actual.getHeight === 80)
+    }
+
+    test("when fitting an image then the resultant image is the expected size") {
+        val image = new BufferedImage(100, 200, BufferedImage.TYPE_INT_RGB)
+        val actual = ImageTools.fit(image, (50, 80))
+        assert(actual.getWidth === 50)
+        assert(actual.getHeight === 80)
+    }
+}
+
