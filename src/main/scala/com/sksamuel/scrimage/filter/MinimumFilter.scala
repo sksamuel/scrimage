29555518f86e42fb1e78133535aa720133479703
sksamuel <sam@sksamuel.com>
2013-06-06 00:58:40 +0100

    Added minimum filter

---
+++ src/main/scala/com/sksamuel/scrimage/filter/MinimumFilter.scala
@@ -0,0 +1,8 @@
+package com.sksamuel.scrimage.filter
+
+import com.sksamuel.scrimage.BufferedOpFilter
+
+/** @author Stephen Samuel */
+object MinimumFilter extends BufferedOpFilter {
+    val op = new com.jhlabs.image.MinimumFilter()
+}
