7b37633993dc27e15e812fef04fed222b67543d4
sksamuel <sam@sksamuel.com>
2013-06-05 23:37:40 +0100

    Added tests for solarize, lens blur and glow

--- src/main/scala/com/sksamuel/scrimage/filter/GaussianBlurFilter.scala
+++ src/main/scala/com/sksamuel/scrimage/filter/GaussianBlurFilter.scala
@@ -3,11 +3,11 @@ package com.sksamuel.scrimage.filter
 import com.sksamuel.scrimage.BufferedOpFilter
 
 /** @author Stephen Samuel */
-class GaussianBlurFilter(radius: Int = 2) extends BufferedOpFilter {
+class GaussianBlurFilter(radius: Int) extends BufferedOpFilter {
     val op = new com.jhlabs.image.GaussianFilter(radius)
 }
 object GaussianBlurFilter {
-    def apply() = new GaussianBlurFilter()
-    def apply(radius: Int) = new GaussianBlurFilter(radius)
+    def apply(): GaussianBlurFilter = apply(2)
+    def apply(radius: Int): GaussianBlurFilter = new GaussianBlurFilter(radius)
 }
 
--- src/main/scala/com/sksamuel/scrimage/filter/GlowFilter.scala
+++ src/main/scala/com/sksamuel/scrimage/filter/GlowFilter.scala
@@ -3,7 +3,11 @@ package com.sksamuel.scrimage.filter
 import com.sksamuel.scrimage.BufferedOpFilter
 
 /** @author Stephen Samuel */
-class GlowFilter(amount: Float = 0.5f) extends BufferedOpFilter {
+class GlowFilter(amount: Float) extends BufferedOpFilter {
     val op = new com.jhlabs.image.GlowFilter()
     op.setAmount(amount)
+}
+object GlowFilter {
+    def apply(): GlowFilter = apply(0.5f)
+    def apply(amount: Float): GlowFilter = new GlowFilter(amount)
 }
