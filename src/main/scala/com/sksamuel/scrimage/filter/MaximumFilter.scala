139dabb7a3f60dd31ea091f3e8887ec7f0ad2787
sksamuel <sam@sksamuel.com>
2013-06-06 00:58:44 +0100

    Added maximum filter

---
+++ src/main/scala/com/sksamuel/scrimage/filter/MaximumFilter.scala
@@ -0,0 +1,8 @@
+package com.sksamuel.scrimage.filter
+
+import com.sksamuel.scrimage.BufferedOpFilter
+
+/** @author Stephen Samuel */
+object MaximumFilter extends BufferedOpFilter {
+    val op = new com.jhlabs.image.MaximumFilter()
+}
