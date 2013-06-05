3d4bccca56acb65063553640ce12aff77d6e5fdd
sksamuel <sam@sksamuel.com>
2013-06-06 00:49:49 +0100

    Added twirl filter

---
+++ src/main/scala/com/sksamuel/scrimage/filter/TwirlFilter.scala
@@ -0,0 +1,17 @@
+package com.sksamuel.scrimage.filter
+
+import com.sksamuel.scrimage.BufferedOpFilter
+
+/** @author Stephen Samuel */
+class TwirlFilter(angle: Double, radius: Int, centerX: Float, centerY: Float) extends BufferedOpFilter {
+    val op = new com.jhlabs.image.TwirlFilter()
+    op.setCentreX(centerX)
+    op.setCentreY(centerY)
+    op.setRadius(radius.toFloat)
+    op.setAngle(angle.toFloat)
+}
+object TwirlFilter {
+    def apply(radius: Int): TwirlFilter = apply(Math.PI / 1.5, radius)
+    def apply(angle: Double, radius: Int, centerX: Float = 0.5f, centerY: Float = 0.5f): TwirlFilter =
+        new TwirlFilter(angle, radius, centerX, centerY)
+}
---
+++ src/test/scala/com/sksamuel/scrimage/filter/TwirlFilterTest.scala
@@ -0,0 +1,15 @@
+package com.sksamuel.scrimage.filter
+
+import org.scalatest.{OneInstancePerTest, BeforeAndAfter, FunSuite}
+import com.sksamuel.scrimage.Image
+
+/** @author Stephen Samuel */
+class TwirlFilterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
+
+    val original = Image(getClass.getResourceAsStream("/bird_small.png"))
+
+    test("filter output matches expected") {
+        val expected = getClass.getResourceAsStream("/bird_small_twirl.png")
+        assert(original.filter(TwirlFilter(150)) === Image(expected))
+    }
+}
