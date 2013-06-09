cd25ea51c6d0104e4015f2e9d5fa242dc8b657d4
sksamuel <sam@sksamuel.com>
2013-06-09 21:57:57 +0100

    Updated tests to not write out images

--- src/test/scala/com/sksamuel/scrimage/filter/ContrastFilterTest.scala
+++ src/test/scala/com/sksamuel/scrimage/filter/ContrastFilterTest.scala
@@ -2,7 +2,6 @@ package com.sksamuel.scrimage.filter
 
 import org.scalatest.{OneInstancePerTest, BeforeAndAfter, FunSuite}
 import com.sksamuel.scrimage.Image
-import java.io.File
 
 /** @author Stephen Samuel */
 class ContrastFilterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
@@ -10,7 +9,6 @@ class ContrastFilterTest extends FunSuite with BeforeAndAfter with OneInstancePe
     val original = Image(getClass.getResourceAsStream("/bird_small.png"))
 
     test("filter output matches expected") {
-        original.filter(ContrastFilter(1.3)).write(new File("src/test/resources/bird_small_contrast.png"))
         val expected = Image(getClass.getResourceAsStream("/bird_small_contrast.png"))
         assert(original.filter(ContrastFilter(1.3)) === expected)
     }
