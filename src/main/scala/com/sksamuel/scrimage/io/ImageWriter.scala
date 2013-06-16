6b6eff9819d14f037663544c038d68417d571db4
sksamuel <sam@sksamuel.com>
2013-06-16 14:16:18 +0100

    Updated image writer for inputstream and byte array methos

--- src/main/scala/com/sksamuel/scrimage/io/ImageWriter.scala
+++ src/main/scala/com/sksamuel/scrimage/io/ImageWriter.scala
@@ -16,21 +16,53 @@
 
 package com.sksamuel.scrimage.io
 
-import java.io.{OutputStream, File, ByteArrayOutputStream}
+import java.io._
 import org.apache.commons.io.FileUtils
 
 /** @author Stephen Samuel */
 trait ImageWriter {
 
+    /**
+     * Writes out this image to the given stream.
+     *
+     * @param out the stream to write out to
+     */
     def write(out: OutputStream)
 
+    /**
+     * Writes out this image to the given filepath.
+     *
+     * @param path the path to write out to.
+     */
     def write(path: String) {
         write(new File(path))
     }
-    def write(bytes: Array[Byte]) {
-        write(new ByteArrayOutputStream())
-    }
+
+    /**
+     * Writes out this image to the given file.
+     *
+     * @param file the file to write out to.
+     */
     def write(file: File) {
         write(FileUtils.openOutputStream(file))
+    }
+
+    /**
+     * Writes out this image to a byte array.
+     *
+     * @return the byte array
+     */
+    def write(): Array[Byte] = {
+        val baos = new ByteArrayOutputStream()
+        write(baos)
+        baos.toByteArray
+    }
+
+    /**
+     * Returns an input stream that reads from this image.
+     */
+    def toStream: InputStream = {
+        val bytes = write()
+        new ByteArrayInputStream(bytes)
     }
 }
