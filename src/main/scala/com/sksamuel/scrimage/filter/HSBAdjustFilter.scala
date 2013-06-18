6f6650828214abc17d79a92e12764ad748b6c670
sksamuel <sam@sksamuel.com>
2013-06-18 09:54:45 +0100

    Added HSB filter

--- src/main/scala/com/sksamuel/scrimage/filter/HSBAdjustFilter.scala
+++ src/main/scala/com/sksamuel/scrimage/filter/HSBAdjustFilter.scala
@@ -2,13 +2,16 @@ package com.sksamuel.scrimage.filter
 
 import com.sksamuel.scrimage.BufferedOpFilter
 
-/** @author Stephen Samuel */
-class HSBFilter(hue: Float, saturation: Float, brightness: Float) extends BufferedOpFilter {
+/** @author Stephen Double */
+class HSBFilter(hue: Double, saturation: Double, brightness: Double) extends BufferedOpFilter {
+    require(hue <= 1)
+    require(brightness <= 1)
+    require(saturation <= 1)
     val op = new thirdparty.jhlabs.image.HSBAdjustFilter()
-    op.setHFactor(hue)
-    op.setSFactor(saturation)
-    op.setBFactor(brightness)
+    op.setHFactor(hue.toFloat)
+    op.setSFactor(saturation.toFloat)
+    op.setBFactor(brightness.toFloat)
 }
 object HSBFilter {
-    def apply(hue: Float, saturation: Float, brightness: Float): HSBFilter = new HSBFilter(hue, saturation, brightness)
+    def apply(hue: Double = 0, saturation: Double = 0, brightness: Double = 0): HSBFilter = new HSBFilter(hue, saturation, brightness)
 }
