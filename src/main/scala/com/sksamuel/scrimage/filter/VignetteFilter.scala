f5837c7113bc138069482892138f8b75f9800d07
sksamuel <sam@sksamuel.com>
2013-06-22 14:10:44 +0100

    Updated licsensing

--- src/main/scala/com/sksamuel/scrimage/filter/VignetteFilter.scala
+++ src/main/scala/com/sksamuel/scrimage/filter/VignetteFilter.scala
@@ -13,7 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-
 package com.sksamuel.scrimage.filter
 
 import com.sksamuel.scrimage.{Image, Filter}
