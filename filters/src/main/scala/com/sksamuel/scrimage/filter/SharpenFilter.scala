c8e82c710b309afcc2adc6c7db231264c3c129fc
sksamuel <sam@sksamuel.com>
2013-07-01 22:21:00 +0100

    Updated license info

--- filters/src/main/scala/com/sksamuel/scrimage/filter/SharpenFilter.scala
+++ filters/src/main/scala/com/sksamuel/scrimage/filter/SharpenFilter.scala
@@ -12,7 +12,8 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
- */package com.sksamuel.scrimage.filter
+ */
+package com.sksamuel.scrimage.filter
 
 import com.sksamuel.scrimage.BufferedOpFilter
 
