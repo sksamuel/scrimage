5a29b8c2849d83e865879e83c560d824d4f51fc0
sksamuel <sam@sksamuel.com>
2013-07-05 23:26:39 +0100

    added opacity filter

---
+++ filters/src/test/scala/com/sksamuel/scrimage/filter/OpacityFilterTest.scala
@@ -0,0 +1,16 @@
+package com.sksamuel.scrimage.filter
+
+import org.scalatest.{OneInstancePerTest, BeforeAndAfter, FunSuite}
+import com.sksamuel.scrimage.Image
+import java.io.File
+
+/** @author Stephen Samuel */
+class OpacityFilterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
+
+    val original = Image(getClass.getResourceAsStream("/bird_small.png"))
+
+    test("opacity filter output matches expected") {
+        val expected = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/filters/bird_small_opacity.png"))
+        assert(original.filter(OpacityFilter(0.5)) === expected)
+    }
+}
