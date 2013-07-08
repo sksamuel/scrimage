1c1ef7c3798f8da6de1f49c3c9b24c28bbd1bdb8
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-07-08 15:43:18 -0700

    deleted crap

--- project/ScrimageBuild.scala
+++ project/ScrimageBuild.scala
@@ -1,6 +1,3 @@
 import com.github.shivawu.sbt.maven.MavenBuild
 
-object ScrimageBuild extends MavenBuild {
-    // "*" is a selector which selects all modules
-    //project("*")
-}
+object ScrimageBuild extends MavenBuild
