acbe7b7b1db132951253ec79addced85dd0f9669
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-08-17 13:13:12 -0700

    stuff

--- core/src/main/scala/com/sksamuel/scrimage/PixelTools.scala
+++ core/src/main/scala/com/sksamuel/scrimage/PixelTools.scala
@@ -17,12 +17,19 @@ package com.sksamuel.scrimage
 
 /** @author Stephen Samuel */
 object PixelTools {
-  def rgb(r: Int, g: Int, b: Int): Int = 
+  def rgb(r: Int, g: Int, b: Int): Int =
     0xFF << 24 | (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF) << 0
-  def argb(a: Int, r: Int, g: Int, b: Int): Int = 
+  def argb(a: Int, r: Int, g: Int, b: Int): Int =
     (a & 0xFF) << 24 | (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF) << 0
   def alpha(pixel: Int): Int = pixel >> 24 & 0xFF
   def red(pixel: Int): Int = pixel >> 16 & 0xFF
   def green(pixel: Int): Int = pixel >> 8 & 0xFF
   def blue(pixel: Int): Int = pixel & 0xFF
+  def gray(pixel: Int): Int =
+    (red(pixel) + green(pixel) + blue(pixel)) / 3
+
+  def scale(factor: Double, pixel: Int): Int = rgb(
+    (factor * red(pixel)).round.toInt,
+    (factor * green(pixel)).round.toInt,
+    (factor * blue(pixel)).round.toInt)
 }
