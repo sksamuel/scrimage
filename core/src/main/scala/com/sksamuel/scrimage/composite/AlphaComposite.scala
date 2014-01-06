233a3134c054b585f1ecd4bda2b79e533c17cd6b
sksamuel <sam@sksamuel.com>
2014-01-06 22:31:45 +0000

    Added alpha composite

---
+++ core/src/main/scala/com/sksamuel/scrimage/composite/AlphaComposite.scala
@@ -0,0 +1,17 @@
+package com.sksamuel.scrimage.composite
+
+import com.sksamuel.scrimage.{Image, Composite}
+import java.awt.Graphics2D
+
+/** @author Stephen Samuel */
+class AlphaComposite(alpha: Double) extends Composite {
+  def apply(src: Image, applicative: Image): Unit = {
+    val g2 = src.awt.getGraphics.asInstanceOf[Graphics2D]
+    g2.setComposite(java.awt.AlphaComposite.SrcOver.derive(alpha.toFloat))
+    g2.drawImage(applicative.awt, 0, 0, null)
+    g2.dispose()
+  }
+}
+object AlphaComposite {
+  def apply(alpha: Double) = new AlphaComposite(alpha)
+}
---
+++ core/src/test/scala/com/sksamuel/scrimage/composite/AlphaCompositeTest.scala
@@ -0,0 +1,23 @@
+package com.sksamuel.scrimage.composite
+
+import org.scalatest.{OneInstancePerTest, BeforeAndAfter, FunSuite}
+import com.sksamuel.scrimage.Image
+
+/** @author Stephen Samuel */
+class AlphaCompositeTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
+
+  val source = Image(getClass.getResourceAsStream("/colosseum.jpg")).resizeTo(400, 300)
+  val transparent = Image(getClass.getResourceAsStream("/transparent_chip.png"))
+  val expected1 = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/composite/alpha_composite.png"))
+  val expected2 = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/composite/alpha_composite_0.5f.png"))
+
+  test("alpha composite uses transparency of application image") {
+    val actual = source.composite(AlphaComposite(1f), transparent)
+    assert(expected1 === actual)
+  }
+
+  test("alpha composite uses transparency of application image combined with alpha") {
+    val actual = source.composite(AlphaComposite(0.5f), transparent)
+    assert(expected2 === actual)
+  }
+}
