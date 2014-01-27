8cf55636990af9f2aad293193395685d8ba3301f
sksamuel <sam@sksamuel.com>
2014-01-27 07:55:40 +0000

    Added shear filter

---
+++ filters/src/main/scala/com/sksamuel/scrimage/filter/ShearFilter.scala
@@ -0,0 +1,14 @@
+package com.sksamuel.scrimage.filter
+
+import com.sksamuel.scrimage.BufferedOpFilter
+
+/** @author Stephen Samuel */
+class ShearFilter(xAngle: Double, yAngle: Double) extends BufferedOpFilter {
+  val op = new thirdparty.jhlabs.image.ShearFilter()
+  op.setXAngle(xAngle.toFloat)
+  op.setYAngle(yAngle.toFloat)
+  op.setResize(false)
+}
+object ShearFilter {
+  def apply(xAngle: Double, yAngle: Double) = new ShearFilter(xAngle, yAngle)
+}
