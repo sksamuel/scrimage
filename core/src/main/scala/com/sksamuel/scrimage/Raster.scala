34d87d21a9a69c5c7fc8980075602578b9fcb416
sksamuel <sam@sksamuel.com>
2014-04-21 00:16:22 +0100

    Added subimage with subraster

--- core/src/main/scala/com/sksamuel/scrimage/Raster.scala
+++ core/src/main/scala/com/sksamuel/scrimage/Raster.scala
@@ -8,19 +8,30 @@ package com.sksamuel.scrimage
  * @author Stephen Samuel
  **/
 trait Raster {
+  val width: Int
+  val height: Int
   def read(x: Int, y: Int): Pixel
   def write(x: Int, y: Int, pixel: Pixel): Unit
+  def subraster(x: Int, y: Int, w: Int, y: Int): Raster = null
+}
+
+object Raster {
+  /**
+   * Create a new default Raster of the specified size.
+   */
+  def apply(width: Int, height: Int): IntARGBRaster = new IntARGBRaster(width, height)
 }
 
 // raster that uses integers to represent all the information in a pixel
-class IntARGBRaster(width: Int, height: Int) extends Raster {
-  private val pixels = Array(width * height)
+class IntARGBRaster(val width: Int, val height: Int) extends Raster {
+  val pixels = Array(width * height)
   override def read(x: Int, y: Int): Pixel = null
   override def write(x: Int, y: Int, pixel: Pixel): Unit = ()
 }
+
 // raster that uses a single byte per sample
-class ByteARGBRaster(width: Int, height: Int) extends Raster {
-  private val pixels = Array(width * height * 4)
+class ByteARGBRaster(val width: Int, val height: Int) extends Raster {
+  val pixels = Array(width * height * 4)
   override def read(x: Int, y: Int): Pixel = null
   override def write(x: Int, y: Int, pixel: Pixel): Unit = ()
 }
