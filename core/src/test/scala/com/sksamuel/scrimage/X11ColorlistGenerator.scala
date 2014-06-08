723af3be37c5a98fcf45e18e0eeeed2e11c0069c
sksamuel <sam@sksamuel.com>
2014-06-08 16:47:23 +0100

    Added X11 colors as constants

---
+++ core/src/test/scala/com/sksamuel/scrimage/X11ColorlistGenerator.scala
@@ -0,0 +1,21 @@
+package com.sksamuel.scrimage
+
+import org.apache.commons.io.IOUtils
+import java.io.FileInputStream
+import scala.collection.JavaConverters._
+import org.apache.commons.lang3.text.WordUtils
+
+/** @author Stephen Samuel */
+object X11ColorlistGenerator extends App {
+  val file = "/etc/X11/rgb.txt"
+  val lines = IOUtils.readLines(new FileInputStream(file))
+  for ( line <- lines.asScala ) {
+    val tokens = line.replaceAll("\\s{2,}", " ").trim.split(' ')
+    val red = tokens(0)
+    val green = tokens(1)
+    val blue = tokens(2)
+    val name = WordUtils.capitalize(tokens.drop(3).mkString(" "))
+    if (!name.contains(" "))
+      println(s"val $name = Color($red, $green, $blue)")
+  }
+}
