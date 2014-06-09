fc188e13ab3e970e9af1e402cb0fc519913ea043
sksamuel <sam@sksamuel.com>
2014-06-09 05:40:32 +0100

    Updated canvas to use helper graphics2d method

--- canvas/src/main/scala/com/sksamuel/scrimage/canvas/Canvas.scala
+++ canvas/src/main/scala/com/sksamuel/scrimage/canvas/Canvas.scala
@@ -7,51 +7,52 @@ import java.awt.{RenderingHints, Font, Graphics2D}
 case class Canvas(image: Image,
                   painter: Painter = X11Colorlist.Black,
                   font: Font = new Font(Font.SERIF, 0, 24),
-                  aliasing: Boolean) {
+                  aliasing: Boolean = false) {
 
   def withPainter(painter: Painter): Canvas = copy(painter = painter)
   def withPainter(color: Color): Canvas = copy(painter = color)
   def withFont(font: Font): Canvas = copy(font = font)
   def withAliasing(aliasing: Boolean) = copy(aliasing = aliasing)
 
-
+  private def g2(image: Image): Graphics2D = {
+    val g2 = image.awt.getGraphics.asInstanceOf[Graphics2D]
+    g2.setPaint(painter.paint)
+    if (aliasing)
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    g2.setFont(font)
+    g2
+  }
 
   def fill: Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.fillRect(0, 0, copy.width, copy.height)
-    g2.dispose()
+    val g = g2(copy)
+    g.fillRect(0, 0, copy.width, copy.height)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def drawLine(x1: Int, y1: Int, x2: Int, y2: Int): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.drawLine(x1, y1, x2, y2)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawLine(x1, y1, x2, y2)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def drawImage(x: Int, y: Int, imageToDraw: Image): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    if (aliasing)
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-    g2.drawImage(imageToDraw.awt, x, y, null)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawImage(imageToDraw.awt, x, y, null)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def fillRect(x: Int, y: Int, w: Int, h: Int): Canvas = fillPoly(Polygon.rectange(x, y, w, h))
   def fillPoly(polygon: Polygon): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.fillPolygon(polygon.points.map(_.x).toArray, polygon.points.map(_.y).toArray, polygon.points.size)
-    g2.dispose()
+    val g = g2(copy)
+    g.fillPolygon(polygon.points.map(_.x).toArray, polygon.points.map(_.y).toArray, polygon.points.size)
+    g.dispose()
     this.copy(image = copy)
   }
 
@@ -62,57 +63,50 @@ case class Canvas(image: Image,
                       arcWidth: Int,
                       arcHeight: Int): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.drawRoundRect(x, y, width, height, arcWidth, arcHeight)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawRoundRect(x, y, width, height, arcWidth, arcHeight)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def fillRoundedRect(x: Int, y: Int, width: Int, height: Int, arcWidth: Int, arcHeight: Int): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.fillRoundRect(x, y, width, height, arcWidth, arcHeight)
-    g2.dispose()
+    val g = g2(copy)
+    g.fillRoundRect(x, y, width, height, arcWidth, arcHeight)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def drawOval(x: Int, y: Int, width: Int, height: Int): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.drawOval(x, y, width, height)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawOval(x, y, width, height)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def fillOval(x: Int, y: Int, width: Int, height: Int): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.fillOval(x, y, width, height)
-    g2.dispose()
+    val g = g2(copy)
+    g.fillOval(x, y, width, height)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def drawRect(x: Int, y: Int, w: Int, h: Int): Canvas = drawPoly(Polygon.rectange(x, y, w, h))
   def drawPoly(polygon: Polygon): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.drawPolygon(polygon.points.map(_.x).toArray, polygon.points.map(_.y).toArray, polygon.points.size)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawPolygon(polygon.points.map(_.x).toArray, polygon.points.map(_.y).toArray, polygon.points.size)
+    g.dispose()
     this.copy(image = copy)
   }
 
   def drawString(x: Int, y: Int, text: String): Canvas = {
     val copy = image.copy
-    val g2 = copy.awt.getGraphics.asInstanceOf[Graphics2D]
-    g2.setPaint(painter.paint)
-    g2.setFont(font)
-    g2.drawString(text, x, y)
-    g2.dispose()
+    val g = g2(copy)
+    g.drawString(text, x, y)
+    g.dispose()
     this.copy(image = copy)
   }
 
