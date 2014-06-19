5bc03a7c690d06ff413d06573ad52a02b2a72ab2
sksamuel <sam@sksamuel.com>
2014-06-19 09:39:21 +0100

    Implemented Int raster

--- core/src/main/scala/com/sksamuel/scrimage/Raster.scala
+++ core/src/main/scala/com/sksamuel/scrimage/Raster.scala
@@ -1,27 +1,60 @@
 package com.sksamuel.scrimage
 
 /**
- *
  * A Raster is a data structure representing a rectangular grid of pixels.
+ * In Scrimage a Raster lies on the plane at 0,0 extending to width,height.
  *
+ * For performance, Rasters are mutable data structures. Thus care should
+ * be taken when sharing between multiple images.
  *
  * @author Stephen Samuel
  **/
 trait Raster {
-  def read(x: Int, y: Int): Pixel
-  def write(x: Int, y: Int, pixel: Pixel): Unit
+
+  val width: Int
+  val height: Int
+
+  /**
+   * Returns the color of the pixel at the given x,y coordinate.
+   *
+   * @param x x-coordinate
+   * @param y y-coordinate
+   *
+   * @return the pixel color
+   */
+  def read(x: Int, y: Int): Color
+
+  /**
+   * Updates (mutates) this raster by setting the color of the pixel for the given x,y coordinate.
+   *
+   * @param x x-coordinate
+   * @param y y-coordinate
+   * @param color the pixel color
+   */
+  def write(x: Int, y: Int, color: Color): Unit
+
+  protected def coordinateToOffset(x: Int, y: Int): Int = y * width + x
+
+  /**
+   * Returns a new Raster whr
+   * @return
+   */
+  def copy: Raster
 }
 
-// raster that uses integers to represent all the information in a pixel
-class IntARGBRaster(width: Int, height: Int) extends Raster {
-  private val pixels = Array(width * height)
-  override def read(x: Int, y: Int): Pixel = null
-  override def write(x: Int, y: Int, pixel: Pixel): Unit = ()
+/**
+ * Implementation of a Raster that packs ARGB information into a single integer.
+ *
+ * @param width number of columns in the raster
+ * @param height number of rows in the raster
+ */
+class IntARGBRaster(val width: Int,
+                    val height: Int,
+                    model: Array[Int] = new Array[Int](width * height)) extends Raster {
+  override def read(x: Int, y: Int): Color = model(coordinateToOffset(x, y))
+  override def write(x: Int, y: Int, color: Color): Unit = {
+    val offset = coordinateToOffset(x, y)
+    model(offset) = color.toInt
+  }
+  def copy: IntARGBRaster = new IntARGBRaster(width, height, model.clone())
 }
-// raster that uses a single byte per sample
-class ByteARGBRaster(width: Int, height: Int) extends Raster {
-  private val pixels = Array(width * height * 4)
-  override def read(x: Int, y: Int): Pixel = null
-  override def write(x: Int, y: Int, pixel: Pixel): Unit = ()
-}
-
