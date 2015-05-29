93cf834f9e9e9c0c9eb7972d00c879de7f14257a
sksamuel <sam@sksamuel.com>
2015-05-30 00:55:54 +0100

    Added io module

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,8 @@ import scalariform.formatter.preferences._
 
 object Build extends Build {
 
+  private val TwelveMonkeysVersion = "3.1.0"
+
   val scrimageSettings = scalariformSettings ++ Seq(
     organization := "com.sksamuel.scrimage",
     name := "scrimage",
@@ -69,7 +71,7 @@ object Build extends Build {
   lazy val root = Project("scrimage", file("."))
     .settings(scrimageSettings: _*)
     .settings(publishArtifact := false)
-    .aggregate(core, filters, metadata)
+    .aggregate(core, filters, metadata, io)
 
   lazy val core = Project("scrimage-core", file("scrimage-core"))
     .settings(scrimageSettings: _*)
@@ -77,9 +79,9 @@ object Build extends Build {
     .settings(
       libraryDependencies ++= Seq(
         "org.apache.sanselan" % "sanselan" % "0.97-incubator",
-        "com.twelvemonkeys.imageio" % "imageio-core" % "3.1.0",
-        "com.twelvemonkeys.imageio" % "imageio-metadata" % "3.1.0",
-        "com.twelvemonkeys.imageio" % "imageio-jpeg" % "3.1.0",
+        "com.twelvemonkeys.imageio" % "imageio-core" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-metadata" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-jpeg" % TwelveMonkeysVersion,
         "com.twelvemonkeys.common" % "common-lang" % "3.1.0",
         "com.twelvemonkeys.common" % "common-io" % "3.1.0",
         "com.twelvemonkeys.common" % "common-image" % "3.1.0",
@@ -87,6 +89,29 @@ object Build extends Build {
         "ar.com.hjg" % "pngj" % "2.1.0",
         "org.apache.commons" % "commons-lang3" % "3.3.2" % "test",
         "com.sksamuel.scam" %% "scam" % "0.5.2" % "test"
+      )
+    )
+
+  lazy val io = Project("scrimage-io", file("scrimage-io"))
+    .settings(scrimageSettings: _*)
+    .settings(name := "scrimage-io")
+    .settings(
+      libraryDependencies ++= Seq(
+        "com.twelvemonkeys.imageio" % "imageio-batik" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-bmp" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-jpeg" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-icns" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-ico" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-iff" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-pcx" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-pict" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-pdf" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-pnm" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-psd" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-sgi" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-tiff" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-tga" % TwelveMonkeysVersion,
+        "com.twelvemonkeys.imageio" % "imageio-thumbsdb" % TwelveMonkeysVersion
       )
     )
 
