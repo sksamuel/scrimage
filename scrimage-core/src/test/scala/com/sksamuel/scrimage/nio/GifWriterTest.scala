d6b99448c60086c56195612fbcaf1c985e142681
sksamuel <sam@sksamuel.com>
2015-05-31 14:46:13 +0100

    GIF Writer tests restored

--- scrimage-core/src/test/scala/com/sksamuel/scrimage/nio/GifWriterTest.scala
+++ scrimage-core/src/test/scala/com/sksamuel/scrimage/nio/GifWriterTest.scala
@@ -1,5 +1,7 @@
 package com.sksamuel.scrimage.nio
 
+import java.io.File
+
 import com.sksamuel.scrimage.Image
 import org.scalatest.{ BeforeAndAfter, FunSuite, OneInstancePerTest }
 
@@ -8,14 +10,14 @@ class GifWriterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest
 
   val original = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/bird.jpg")).scaleTo(300, 200)
 
-  ignore("GIF output happy path") {
+  test("GIF output happy path") {
     val actual = Image(original.bytes(GifWriter.Default))
     val expected = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/io/bird_compressed.gif"))
     assert(expected.pixels.length === actual.pixels.length)
     assert(expected == actual)
   }
 
-  ignore("GIF progressive output happy path") {
+  test("GIF progressive output happy path") {
     val actual = Image(original.bytes(GifWriter.Progressive))
     val expected = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/io/bird_progressive.gif"))
     assert(expected.pixels.length === actual.pixels.length)
