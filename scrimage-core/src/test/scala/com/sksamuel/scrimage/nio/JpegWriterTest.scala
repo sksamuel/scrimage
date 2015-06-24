3b295485e0accb9727a1493ef20d3796f5bade7b
sksamuel <sam@sksamuel.com>
2015-06-24 21:09:38 +0100

    Adding test for issue #84 and adding travis build for openjdk8

--- scrimage-core/src/test/scala/com/sksamuel/scrimage/nio/JpegWriterTest.scala
+++ scrimage-core/src/test/scala/com/sksamuel/scrimage/nio/JpegWriterTest.scala
@@ -1,18 +1,22 @@
 package com.sksamuel.scrimage.nio
 
 import com.sksamuel.scrimage.Image
-import org.scalatest.{ BeforeAndAfter, FunSuite, OneInstancePerTest }
+import org.scalatest.{BeforeAndAfter, FunSuite, OneInstancePerTest}
 
 /** @author Stephen Samuel */
 class JpegWriterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  val original = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/bird.jpg")).scaleTo(600, 400)
+  val original = Image.fromResource("/com/sksamuel/scrimage/bird.jpg").scaleTo(600, 400)
 
   test("jpeg compression happy path") {
-    for (i <- 0 to 100 by 10) {
+    for ( i <- 0 to 100 by 10 ) {
       original.bytes(JpegWriter().withCompression(i)) // make sure no exceptions for each format level
     }
+  }
+
+  test("issue 84 - jpeg writing with alpha") {
+    val img = Image.fromResource("/com/sksamuel/scrimage/nio/issue84.jpg")
+    val w = JpegWriter()
+    img.bytes(w) // was throwing with bug
   }
 }
