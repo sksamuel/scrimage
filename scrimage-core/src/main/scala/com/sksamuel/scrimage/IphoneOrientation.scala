d34dd22c5bb893cfb7b6ae7f0c9810c4c17dd4f1
sksamuel <sam@sksamuel.com>
2015-06-27 12:16:07 +0100

    #47 added tests for all 4 orientations on iphone

--- scrimage-core/src/main/scala/com/sksamuel/scrimage/IphoneOrientation.scala
+++ scrimage-core/src/main/scala/com/sksamuel/scrimage/IphoneOrientation.scala
@@ -4,8 +4,10 @@ object IphoneOrientation {
 
   def reorient(image: Image, metadata: ImageMetadata): Image = {
     metadata.tag(274).fold(image)(tag => tag.rawValue match {
-      case "6" => image.rotateLeft
-      case _ => image
+      case "3" => image.flipY // Bottom, right side (Rotate 180)
+      case "6" => image.rotateLeft // 6,Right side, top (Rotate 90 CW)))
+      case "8" => image.rotateRight // 8,Left side, bottom (Rotate 270 CW)
+      case _ => image // no info or 1,Top, left side (Horizontal / normal)))
     })
   }
 }
