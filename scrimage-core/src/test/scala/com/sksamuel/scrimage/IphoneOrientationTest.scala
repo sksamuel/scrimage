62f19e59034291afc85f96599b963930188f6c05
Henk Erik van der Hoek <hevanderhoek@xebia.com>
2016-08-09 16:44:40 +0200

    Correctly handle all exif orientation flags.

--- scrimage-core/src/test/scala/com/sksamuel/scrimage/IphoneOrientationTest.scala
+++ scrimage-core/src/test/scala/com/sksamuel/scrimage/IphoneOrientationTest.scala
@@ -1,27 +1,42 @@
 package com.sksamuel.scrimage
 
-import org.apache.commons.io.IOUtils
-import org.scalatest.{WordSpec, Matchers}
+import java.io.FileOutputStream
+
+import com.sksamuel.scrimage.nio.JpegWriter
+import org.apache.commons.io.{FileUtils, IOUtils}
+import org.scalatest.{Matchers, WordSpec}
 
 class IphoneOrientationTest extends WordSpec with Matchers {
 
   "iphone image" should {
     "be re-orientated" in {
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_1.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_1_expected.jpg"))
 
-      Image.fromResource("/com/sksamuel/scrimage/iphone/left.JPG").scale(0.25).bytes shouldBe
-        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/left_reorientated.png"))
-      Image.fromResource("/com/sksamuel/scrimage/iphone/left.JPG").width shouldBe 960
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_2.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_2_expected.jpg"))
 
-      Image.fromResource("/com/sksamuel/scrimage/iphone/down.JPG").scale(0.25).bytes shouldBe
-        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/down_reorientated.png"))
-      Image.fromResource("/com/sksamuel/scrimage/iphone/down.JPG").width shouldBe 1280
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_3.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_3_expected.jpg"))
 
-      Image.fromResource("/com/sksamuel/scrimage/iphone/right.JPG").scale(0.25).bytes shouldBe
-        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/right_reorientated.png"))
-      Image.fromResource("/com/sksamuel/scrimage/iphone/right.JPG").width shouldBe 960
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_4.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_4_expected.jpg"))
+
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_5.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_5_expected.jpg"))
+
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_6.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_6_expected.jpg"))
+
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_7.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_7_expected.jpg"))
+
+      Image.fromResource("/com/sksamuel/scrimage/iphone/portrait_8.jpg").bytes(new JpegWriter(100, true)) shouldBe
+        IOUtils.toByteArray(getClass.getResourceAsStream("/com/sksamuel/scrimage/iphone/portrait_8_expected.jpg"));
 
       Image.fromResource("/com/sksamuel/scrimage/iphone/up.JPG").width shouldBe 1280
     }
+
     "orientate for multiple same-value tags #93" in {
       Image.fromResource("/issue93.jpg").width shouldBe 2160
     }
