a2a2bd15ef7b36d9b273cff03bec8413c1b12405
sksamuel <sam@sksamuel.com>
2017-06-19 05:45:40 +0100

    Added filter / io projects back into build for alpha

--- project/Build.scala
+++ project/Build.scala
@@ -63,7 +63,7 @@ object Build extends Build {
   lazy val root = Project("scrimage", file("."))
     .settings(scrimageSettings: _*)
     .settings(publishArtifact := false)
-    .aggregate(core)
+    .aggregate(core, io, filters)
 
   lazy val core = Project("scrimage-core", file("scrimage-core"))
     .settings(scrimageSettings: _*)
