c6473686e77ee5224ff43ab652da68c2cbf8e847
sksamuel <sam@sksamuel.com>
2017-09-14 13:00:17 +0100

    Updated composites to fix weird compile bug in 2.12

--- scrimage-filters/src/main/scala/thirdparty/jhlabs/composite/RGBComposite.java
+++ scrimage-filters/src/main/scala/thirdparty/jhlabs/composite/RGBComposite.java
@@ -17,9 +17,6 @@ limitations under the License.
 package thirdparty.jhlabs.composite;
 
 import java.awt.*;
-import java.awt.image.ColorModel;
-import java.awt.image.Raster;
-import java.awt.image.WritableRaster;
 
 public abstract class RGBComposite implements Composite {
 
@@ -46,41 +43,5 @@ public abstract class RGBComposite implements Composite {
         return extraAlpha == c.extraAlpha;
     }
 
-    public abstract static class RGBCompositeContext implements CompositeContext {
 
-        private float alpha;
-        private ColorModel srcColorModel;
-        private ColorModel dstColorModel;
-
-        public RGBCompositeContext(float alpha, ColorModel srcColorModel, ColorModel dstColorModel) {
-            this.alpha = alpha;
-            this.srcColorModel = srcColorModel;
-            this.dstColorModel = dstColorModel;
-        }
-
-        public void dispose() {
-        }
-
-        public abstract void composeRGB(int[] src, int[] dst, float alpha);
-
-        public void compose(Raster src, Raster dstIn, WritableRaster dstOut) {
-            float alpha = this.alpha;
-
-            int[] srcPix = null;
-            int[] dstPix = null;
-
-            int x = dstOut.getMinX();
-            int w = dstOut.getWidth();
-            int y0 = dstOut.getMinY();
-            int y1 = y0 + dstOut.getHeight();
-
-            for (int y = y0; y < y1; y++) {
-                srcPix = src.getPixels(x, y, w, 1, srcPix);
-                dstPix = dstIn.getPixels(x, y, w, 1, dstPix);
-                composeRGB(srcPix, dstPix, alpha);
-                dstOut.setPixels(x, y, w, 1, dstPix);
-            }
-        }
-
-    }
 }
--- scrimage-filters/src/main/scala/thirdparty/jhlabs/composite/SubtractComposite.java
+++ scrimage-filters/src/main/scala/thirdparty/jhlabs/composite/SubtractComposite.java
@@ -21,52 +21,45 @@ import java.awt.image.*;
 
 public final class SubtractComposite extends RGBComposite {
 
-	public SubtractComposite( float alpha ) {
-        super( alpha );
-	}
-
-	public CompositeContext createContext( ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints ) {
-		return new Context( extraAlpha, srcColorModel, dstColorModel );
-	}
-
-    static class Context extends RGBCompositeContext {
-        public Context( float alpha, ColorModel srcColorModel, ColorModel dstColorModel ) {
-            super( alpha, srcColorModel, dstColorModel );
-        }
-
-        public void composeRGB( int[] src, int[] dst, float alpha ) {
-            int w = src.length;
-
-            for ( int i = 0; i < w; i += 4 ) {
-                int sr = src[i];
-                int dir = dst[i];
-                int sg = src[i+1];
-                int dig = dst[i+1];
-                int sb = src[i+2];
-                int dib = dst[i+2];
-                int sa = src[i+3];
-                int dia = dst[i+3];
-                int dor, dog, dob;
-
-                dor = dir - sr;
-                if ( dor < 0 )
-                    dor = 0;
-                dog = dig - sg;
-                if ( dog < 0 )
-                    dog = 0;
-                dob = dib - sb;
-                if ( dob < 0 )
-                    dob = 0;
-
-                float a = alpha*sa/255f;
-                float ac = 1-a;
-
-                dst[i] = (int)(a*dor + ac*dir);
-                dst[i+1] = (int)(a*dog + ac*dig);
-                dst[i+2] = (int)(a*dob + ac*dib);
-                dst[i+3] = (int)(sa*alpha + dia*ac);
-            }
-        }
+    public SubtractComposite(float alpha) {
+        super(alpha);
     }
 
+    public CompositeContext createContext(ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints) {
+        return new RGBCompositeContext(extraAlpha, srcColorModel, dstColorModel) {
+            public void composeRGB(int[] src, int[] dst, float alpha) {
+                int w = src.length;
+
+                for (int i = 0; i < w; i += 4) {
+                    int sr = src[i];
+                    int dir = dst[i];
+                    int sg = src[i + 1];
+                    int dig = dst[i + 1];
+                    int sb = src[i + 2];
+                    int dib = dst[i + 2];
+                    int sa = src[i + 3];
+                    int dia = dst[i + 3];
+                    int dor, dog, dob;
+
+                    dor = dir - sr;
+                    if (dor < 0)
+                        dor = 0;
+                    dog = dig - sg;
+                    if (dog < 0)
+                        dog = 0;
+                    dob = dib - sb;
+                    if (dob < 0)
+                        dob = 0;
+
+                    float a = alpha * sa / 255f;
+                    float ac = 1 - a;
+
+                    dst[i] = (int) (a * dor + ac * dir);
+                    dst[i + 1] = (int) (a * dog + ac * dig);
+                    dst[i + 2] = (int) (a * dob + ac * dib);
+                    dst[i + 3] = (int) (sa * alpha + dia * ac);
+                }
+            }
+        };
+    }
 }
