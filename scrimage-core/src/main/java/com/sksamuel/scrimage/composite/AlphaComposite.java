aa75b21a6f2e8882d361b87d75880cac56ad15e7
sksamuel <sam@sksamuel.com>
2017-12-23 17:00:49 +0000

    Migrated alpha composite to java

---
+++ scrimage-core/src/main/java/com/sksamuel/scrimage/composite/AlphaComposite.java
@@ -0,0 +1,23 @@
+package com.sksamuel.scrimage.composite;
+
+import com.sksamuel.scrimage.AwtImage;
+import com.sksamuel.scrimage.Composite;
+
+import java.awt.*;
+
+public class AlphaComposite implements Composite {
+
+    private final double alpha;
+
+    public AlphaComposite(double alpha) {
+        this.alpha = alpha;
+    }
+
+    @Override
+    public void apply(AwtImage src, AwtImage overlay) {
+        Graphics2D g2 = (Graphics2D) src.awt().getGraphics();
+        g2.setComposite(java.awt.AlphaComposite.SrcOver.derive((float) alpha));
+        g2.drawImage(overlay.awt(), 0, 0, null);
+        g2.dispose();
+    }
+}
