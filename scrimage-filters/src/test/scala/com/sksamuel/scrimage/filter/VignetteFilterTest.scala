3ab62bb19a68219e7a76d53e68b838ab4b70d300
sksamuel <sam@sksamuel.com>
2017-12-24 11:11:25 +0000

    Fixed vignetter/summer tests

--- scrimage-filters/src/test/scala/com/sksamuel/scrimage/filter/VignetteFilterTest.scala
+++ scrimage-filters/src/test/scala/com/sksamuel/scrimage/filter/VignetteFilterTest.scala
@@ -3,13 +3,12 @@ package com.sksamuel.scrimage.filter
 import com.sksamuel.scrimage.Image
 import org.scalatest.FunSuite
 
-/** @author Stephen Samuel */
 class VignetteFilterTest extends FunSuite {
 
-  val original = Image(getClass.getResourceAsStream("/bird_small.png"))
+  private val original = Image.fromResource("/bird_small.png")
 
   test("filter output matches expected") {
-    val expected = Image(getClass.getResourceAsStream("/com/sksamuel/scrimage/filters/bird_small_vignette.png"))
+    val expected = Image.fromResource("/com/sksamuel/scrimage/filters/bird_small_vignette.png")
     val actual = original.filter(new VignetteFilter())
     assert(actual === expected)
   }
