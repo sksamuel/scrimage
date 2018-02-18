35f7ed2e62334c3d203a5900a415f2768d99cff5
sksamuel <sam@sksamuel.com>
2018-02-18 14:48:09 +0000

    Added extra tests for blur filter

--- scrimage-filters/src/test/scala/com/sksamuel/scrimage/filter/BlurFilterTest.scala
+++ scrimage-filters/src/test/scala/com/sksamuel/scrimage/filter/BlurFilterTest.scala
@@ -1,11 +1,18 @@
 package com.sksamuel.scrimage.filter
 
 import com.sksamuel.scrimage.Image
-import org.scalatest.{BeforeAndAfter, FunSuite, OneInstancePerTest}
+import com.sksamuel.scrimage.nio.PngWriter
+import org.scalatest.FunSuite
 
-class BlurFilterTest extends FunSuite with BeforeAndAfter with OneInstancePerTest {
+class BlurFilterTest extends FunSuite {
+
+  implicit val writer: PngWriter = PngWriter.MaxCompression
+  private val folder = "/com/sksamuel/scrimage/filters/blur"
 
   test("filter output matches expected") {
-    assert(Image.fromResource("/bird_small.png").filter(new BlurFilter) == Image.fromResource("/com/sksamuel/scrimage/filters/bird_small_blur.png"))
+    assert(Image.fromResource("/bird_small.png").filter(new BlurFilter) == Image.fromResource(s"$folder/bird_small_blur.png"))
+    assert(Image.fromResource("/caviar.jpg").filter(new BlurFilter) == Image.fromResource(s"$folder/caviar_blur.png"))
+    assert(Image.fromResource("/love.jpg").filter(new BlurFilter) == Image.fromResource(s"$folder/love_blur.png"))
+    assert(Image.fromResource("/masks.jpg").filter(new BlurFilter) == Image.fromResource(s"$folder/masks_blur.png"))
   }
 }
