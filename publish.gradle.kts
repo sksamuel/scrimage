fbb35bf7fd8f4bdc3ac1e8cad87a69c1766c0ba6
sksamuel <sam@sksamuel.com>
2020-02-22 12:20:28 -0600

    Added publishing to github workflow

---
+++ publish.gradle.kts
@@ -0,0 +1,72 @@
+apply(plugin = "java")
+apply(plugin = "java-library")
+apply(plugin = "maven-publish")
+apply(plugin = "signing")
+
+repositories {
+   mavenCentral()
+}
+
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+
+fun Project.publishing(action: PublishingExtension.() -> Unit) =
+   configure(action)
+
+fun Project.signing(configure: SigningExtension.() -> Unit): Unit =
+   configure(configure)
+
+val publications: PublicationContainer = (extensions.getByName("publishing") as PublishingExtension).publications
+
+signing {
+   useGpgCmd()
+   if (Ci.isReleaseVersion)
+      sign(publications)
+}
+
+publishing {
+   repositories {
+      maven {
+         val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+         val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+         name = "deploy"
+         url = if (Ci.isGithub) snapshotsRepoUrl else releasesRepoUrl
+         credentials {
+            username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
+            password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
+         }
+      }
+   }
+
+   publications.withType<MavenPublication>().forEach {
+      it.apply {
+         artifact(dokkaJar)
+         pom {
+            name.set("Scrimage")
+            description.set("JVM Image Library")
+            url.set("http://www.github.com/sksamuel/scrimage")
+
+            scm {
+               connection.set("scm:git:http://www.github.com/sksamuel/scrimage/")
+               developerConnection.set("scm:git:http://github.com/sksamuel/")
+               url.set("http://www.github.com/sksamuel/scrimage/")
+            }
+
+            licenses {
+               license {
+                  name.set("The Apache 2.0 License")
+                  url.set("https://opensource.org/licenses/Apache-2.0")
+               }
+            }
+
+            developers {
+               developer {
+                  id.set("sksamuel")
+                  name.set("Stephen Samuel")
+                  email.set("sam@sksamuel.com")
+               }
+            }
+         }
+      }
+   }
+}
--- snapshot_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" == 'master' ]; then
-  sbt +publish
-fi
