2a4023f1705de3257eb69861bde44abadb170740
sksamuel <sam@sksamuel.com>
2020-02-23 22:49:25 -0600

    Added publication for scala modules

--- scrimage-scala_2.12/build.gradle.kts
+++ scrimage-scala_2.12/build.gradle.kts
@@ -6,3 +6,5 @@ dependencies {
    implementation("org.scala-lang:scala-library:2.12.10")
    implementation(project(":scrimage-core"))
 }
+
+apply("../publish.gradle.kts")
--- scrimage-scala_2.13/build.gradle.kts
+++ scrimage-scala_2.13/build.gradle.kts
@@ -6,3 +6,5 @@ dependencies {
    implementation("org.scala-lang:scala-library:2.13.1")
    implementation(project(":scrimage-core"))
 }
+
+apply("../publish.gradle.kts")
