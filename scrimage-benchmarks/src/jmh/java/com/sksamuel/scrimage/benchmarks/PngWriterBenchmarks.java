45505b1279b173dd53438471a2f598107d9b5df7
sksamuel <sam@sksamuel.com>
2020-10-12 21:21:06 -0500

    Added benchmark for png writer

---
+++ scrimage-benchmarks/src/jmh/java/com/sksamuel/scrimage/benchmarks/PngWriterBenchmarks.java
@@ -0,0 +1,24 @@
+package com.sksamuel.scrimage.benchmarks;
+
+import com.sksamuel.scrimage.ImmutableImage;
+import com.sksamuel.scrimage.nio.PngWriter;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class PngWriterBenchmarks {
+
+   @Benchmark
+   @BenchmarkMode(Mode.AverageTime)
+   @OutputTimeUnit(TimeUnit.MILLISECONDS)
+   public void testScrimagePngWriterNoCompression(Blackhole blackhole) throws IOException {
+      ImmutableImage image = ImmutableImage.loader().fromResource("/bird.jpg");
+      byte[] png = image.bytes(PngWriter.NoCompression);
+      blackhole.consume(png);
+   }
+}
