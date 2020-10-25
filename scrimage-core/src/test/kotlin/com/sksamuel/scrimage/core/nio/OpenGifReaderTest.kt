60e0eea34ed631349afecd3ee74b12b52adc564f
sksamuel <sam@sksamuel.com>
2020-10-25 14:13:29 -0500

    #211 Added gif test

---
+++ scrimage-core/src/test/kotlin/com/sksamuel/scrimage/core/nio/OpenGifReaderTest.kt
@@ -0,0 +1,19 @@
+package com.sksamuel.scrimage.core.nio
+
+import com.sksamuel.scrimage.nio.OpenGifReader
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class OpenGifReaderTest : FunSpec() {
+   init {
+      test("reading all frames of an animated gif") {
+         val frames = OpenGifReader().readAll(this::class.java.getResourceAsStream("/animated.gif").readAllBytes())
+         frames.size shouldBe 34
+         frames.forAll {
+            it.width shouldBe 648
+            it.height shouldBe 648
+         }
+      }
+   }
+}
