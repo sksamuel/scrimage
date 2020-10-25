fc45dbece2c3d29784fcfe03b5c50f4c4fb8c004
sksamuel <sam@sksamuel.com>
2020-10-25 14:10:31 -0500

    #211 Added multi frame gif reader

--- scrimage-core/src/main/java/com/sksamuel/scrimage/nio/OpenGifReader.java
+++ scrimage-core/src/main/java/com/sksamuel/scrimage/nio/OpenGifReader.java
@@ -5,6 +5,8 @@ import com.sksamuel.scrimage.ImmutableImage;
 
 import java.awt.Rectangle;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OpenGifReader implements ImageReader {
 
@@ -16,5 +18,18 @@ public class OpenGifReader implements ImageReader {
          image = image.subimage(rectangle);
       }
       return image;
+   }
+
+   /**
+    * Returns all frames from a GIF.
+    */
+   public List<ImmutableImage> readAll(byte[] bytes) throws IOException {
+      final GifDecoder.GifImage gif = GifDecoder.read(bytes);
+      List<ImmutableImage> images = new ArrayList<>(gif.getFrameCount());
+      for (int k = 0; k < gif.getFrameCount(); k++) {
+         ImmutableImage image = ImmutableImage.wrapAwt(gif.getFrame(k));
+         images.add(image);
+      }
+      return images;
    }
 }
