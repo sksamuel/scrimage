f26c4a76ce6acd53c54f27e4f8a3c4220a23acf3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-25 17:20:19 +0900

    docs: javadoc to remove another module class

--- scrimage-core/src/main/java/com/sksamuel/scrimage/nio/AnimatedImageWriter.java
+++ scrimage-core/src/main/java/com/sksamuel/scrimage/nio/AnimatedImageWriter.java
@@ -6,7 +6,6 @@ import java.io.OutputStream;
 /**
  * Interface supporting writing of an Image to an array of bytes in a gif
  *
- * @see com.sksamuel.scrimage.webp.Gif2WebpWriter
  */
 public interface AnimatedImageWriter {
    void write(AnimatedGif gif, OutputStream out) throws IOException;
