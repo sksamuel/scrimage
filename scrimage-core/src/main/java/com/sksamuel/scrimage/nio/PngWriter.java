8f60de77c797fe71151d8ea71f99a83fa557f2f2
sksamuel <sam@sksamuel.com>
2023-06-10 20:06:17 -0500

    Fix PNG image compression PNG image compression

--- scrimage-core/src/main/java/com/sksamuel/scrimage/nio/PngWriter.java
+++ scrimage-core/src/main/java/com/sksamuel/scrimage/nio/PngWriter.java
@@ -16,88 +16,76 @@
 
 package com.sksamuel.scrimage.nio;
 
-import ar.com.hjg.pngj.FilterType;
-import ar.com.hjg.pngj.ImageInfo;
-import ar.com.hjg.pngj.ImageLineInt;
 import com.sksamuel.scrimage.AwtImage;
 import com.sksamuel.scrimage.metadata.ImageMetadata;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
-import java.awt.image.SinglePixelPackedSampleModel;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class PngWriter implements ImageWriter {
 
-    public static final PngWriter MaxCompression = new PngWriter(9);
-    public static final PngWriter MinCompression = new PngWriter(1);
-    public static final PngWriter NoCompression = new PngWriter(0);
+   public static final PngWriter MaxCompression = new PngWriter(9);
+   public static final PngWriter MinCompression = new PngWriter(1);
+   public static final PngWriter NoCompression = new PngWriter(0);
 
-    private final int compressionLevel;
+   private final int compressionLevel;
 
-    public PngWriter() {
-        this.compressionLevel = 9;
-    }
+   public PngWriter() {
+      this.compressionLevel = 9;
+   }
 
-    public PngWriter(int compressionLevel) {
-        this.compressionLevel = compressionLevel;
-    }
+   public PngWriter(int compressionLevel) {
+      this.compressionLevel = compressionLevel;
+   }
 
-    // require(compressionLevel >= 0 && compressionLevel < 10, "Compression level must be between 0 (none) and 9 (max)")
+   // require(compressionLevel >= 0 && compressionLevel < 10, "Compression level must be between 0 (none) and 9 (max)")
 
-    public PngWriter withMaxCompression() {
-        return MaxCompression;
-    }
+   public PngWriter withMaxCompression() {
+      return MaxCompression;
+   }
 
-    public PngWriter withMinCompression() {
-        return MinCompression;
-    }
+   public PngWriter withMinCompression() {
+      return MinCompression;
+   }
 
-    public PngWriter withCompression(int compression) {
-        return new PngWriter(compression);
-    }
+   public PngWriter withCompression(int compression) {
+      return new PngWriter(compression);
+   }
 
-    @Override
-    public void write(AwtImage image, ImageMetadata metadata, OutputStream out) throws IOException {
+   @Override
+   public void write(AwtImage image, ImageMetadata metadata, OutputStream out) throws IOException {
 
-        if (image.awt().getType() == BufferedImage.TYPE_INT_ARGB) {
+      ImageTypeSpecifier type = ImageTypeSpecifier.createFromBufferedImageType(image.getType());
+      javax.imageio.ImageWriter writer = ImageIO.getImageWriters(type, "png").next();
+      ImageWriteParam param = writer.getDefaultWriteParam();
 
-            ImageInfo imi = new ImageInfo(image.width, image.height, 8, true);
+      if (param.canWriteCompressed()) {
+         switch (compressionLevel) {
+            case 9: // max
+               param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+               param.setCompressionQuality(0.0f);
+               break;
+            case 1: // min
+               param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+               param.setCompressionQuality(1.0f);
+               break;
+            case 0: // none
+               param.setCompressionMode(ImageWriteParam.MODE_DISABLED);
+            default:
+               param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+               param.setCompressionQuality(compressionLevel / 10f);
+         }
+      }
 
-            ar.com.hjg.pngj.PngWriter writer = new ar.com.hjg.pngj.PngWriter(out, imi);
-            writer.setCompLevel(compressionLevel);
-            writer.setFilterType(FilterType.FILTER_DEFAULT);
-
-            DataBufferInt db = (DataBufferInt) image.awt().getRaster().getDataBuffer();
-            if (db.getNumBanks() != 1) throw new RuntimeException("This method expects one bank");
-
-            SinglePixelPackedSampleModel samplemodel = (SinglePixelPackedSampleModel) image.awt().getSampleModel();
-            ImageLineInt line = new ImageLineInt(imi);
-            int[] dbbuf = db.getData();
-
-            for (int row = 0; row < imi.rows; row++) {
-                int elem = samplemodel.getOffset(0, row);
-                int j = 0;
-                for (int col = 0; col < imi.cols; col++) {
-                    int sample = dbbuf[elem];
-                    elem = elem + 1;
-                    line.getScanline()[j] = (sample & 0xFF0000) >> 16; // R
-                    j = j + 1;
-                    line.getScanline()[j] = (sample & 0xFF00) >> 8; // G
-                    j = j + 1;
-                    line.getScanline()[j] = sample & 0xFF; // B
-                    j = j + 1;
-                    line.getScanline()[j] = ((sample & 0xFF000000) >> 24) & 0xFF; // A
-                    j = j + 1;
-                }
-                writer.writeRow(line, row);
-            }
-            writer.end();// end calls close
-
-        } else {
-            ImageIO.write(image.awt(), "png", out);
-        }
-    }
+      ImageOutputStream ios = ImageIO.createImageOutputStream(out);
+      writer.setOutput(ios);
+      writer.write(null, new IIOImage(image.awt(), null, null), param);
+      writer.dispose();
+      ios.close();
+   }
 }
