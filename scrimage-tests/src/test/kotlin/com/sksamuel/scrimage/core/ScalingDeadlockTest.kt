2ca50e7e56fc491911e476b37cdde271f2f494fe
sksamuel <sam@sksamuel.com>
2023-11-04 14:24:26 -0500

    Use CopyOnWriteArrayList for ScalingDeadlockTest

--- scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/ScalingDeadlockTest.kt
+++ scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/ScalingDeadlockTest.kt
@@ -6,6 +6,7 @@ import com.sksamuel.scrimage.ImmutableImage
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
+import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.CountDownLatch
 import kotlin.concurrent.thread
 
@@ -14,7 +15,7 @@ class ScalingDeadlockTest : FunSpec({
    val image = ImmutableImage.loader().fromStream(javaClass.getResourceAsStream("/com/sksamuel/scrimage/bird.jpg"))
 
    test("image scale should not deadlock on multiple concurrent scales") {
-      val images = mutableListOf<ImmutableImage>()
+      val images = CopyOnWriteArrayList<ImmutableImage>()
       val latch = CountDownLatch(50)
       repeat(50) {
          thread {
