eec5e985054dee3890f3d393851a7211921b5ffa
sksamuel <sam@sksamuel.com>
2025-07-17 08:23:05 -0500

    Added loader from URL

--- scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/Issue89Test.kt
+++ scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/Issue89Test.kt
@@ -10,11 +10,11 @@ class Issue89Test : WordSpec({
    "Loading image" should {
       "not randomly rotate"  {
          repeat(10) {
-            val image = ImmutableImage.fromResource("/issue89.jpg")
+            val image = ImmutableImage.loader().fromResource("/issue89.jpg")
             image.width shouldBe 319
             image.height shouldBe 397
             val bytes = image.bytes(JpegWriter())
-            val reread = ImmutableImage.parse(bytes)
+            val reread = ImmutableImage.loader().fromBytes(bytes)
             reread.width shouldBe 319
             reread.height shouldBe 397
          }
