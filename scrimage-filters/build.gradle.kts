bd458567ca435bbc633399586e35358e03710357
sksamuel <sam@sksamuel.com>
2025-07-19 03:38:20 -0500

    Bump commons io

--- scrimage-filters/build.gradle.kts
+++ scrimage-filters/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
    implementation(project(":scrimage-core"))
-   implementation("commons-io:commons-io:2.11.0")
+   implementation("commons-io:commons-io:2.19.0")
    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
 }
