08c45a340d061eeae8694bcfb0040aaed502b0bb
sksamuel <sam@sksamuel.com>
2025-07-19 03:36:04 -0500

    Move kotlin into test conventions

--- buildSrc/src/main/kotlin/jvm-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jvm-conventions.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
    java
-   kotlin("jvm")
 }
 
 java {
@@ -11,16 +10,4 @@ java {
    withSourcesJar()
    sourceCompatibility = JavaVersion.VERSION_1_8
    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-dependencies {
-   testImplementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
-}
-
-kotlin {
-   compilerOptions {
-      jvmTarget.set(JvmTarget.JVM_1_8)
-      apiVersion.set(KotlinVersion.KOTLIN_2_1)
-      languageVersion.set(KotlinVersion.KOTLIN_2_1)
-   }
 }
--- buildSrc/src/main/kotlin/testing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/testing-conventions.gradle.kts
@@ -1,5 +1,21 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
    id("jvm-conventions")
+   kotlin("jvm")
+}
+
+dependencies {
+   testImplementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
+}
+
+kotlin {
+   compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_1_8)
+      apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
+      languageVersion.set(KotlinVersion.KOTLIN_2_1)
+   }
 }
 
 tasks.named<Test>("test") {
