4e9745ca2667678771dbca25aa2112958d7ba8cf
sksamuel <sam@sksamuel.com>
2025-07-19 03:39:53 -0500

    Update TwelveMonkeys dependencies to version 3.12.0
    Upgraded TwelveMonkeys ImageIO dependencies from version 3.9.4 to 3.12.0 across multiple modules. This ensures compatibility with the latest features and fixes provided by the library.

--- scrimage-tests/build.gradle.kts
+++ scrimage-tests/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 dependencies {
    implementation(project(":scrimage-core"))
    implementation(project(":scrimage-filters"))
-   implementation("com.twelvemonkeys.imageio:imageio-core:3.9.4")
-   implementation("com.twelvemonkeys.imageio:imageio-jpeg:3.9.4")
+   implementation("com.twelvemonkeys.imageio:imageio-core:3.12.0")
+   implementation("com.twelvemonkeys.imageio:imageio-jpeg:3.12.0")
    implementation("com.drewnoakes:metadata-extractor:2.18.0")
    implementation("commons-io:commons-io:2.11.0")
    testImplementation("io.kotest:kotest-framework-datatest:5.5.5")
