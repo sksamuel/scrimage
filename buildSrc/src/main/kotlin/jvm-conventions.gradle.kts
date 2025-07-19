e1d5b5e0e7666e5cc233afb6e05ec8fe358afe5b
sksamuel <sam@sksamuel.com>
2025-07-19 03:57:26 -0500

    Removed withSourcesJar as the publish plugin does this

--- buildSrc/src/main/kotlin/jvm-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jvm-conventions.gradle.kts
@@ -1,12 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-
 plugins {
    java
 }
 
 java {
-   withSourcesJar()
    sourceCompatibility = JavaVersion.VERSION_1_8
    targetCompatibility = JavaVersion.VERSION_1_8
 }
