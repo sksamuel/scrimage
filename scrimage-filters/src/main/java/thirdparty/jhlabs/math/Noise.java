506426be69a63a31956d01b33bfdc302b068a53b
sksamuel <sam@sksamuel.com>
2025-07-19 03:53:50 -0500

    Removed unused code

--- scrimage-filters/src/main/java/thirdparty/jhlabs/math/Noise.java
+++ scrimage-filters/src/main/java/thirdparty/jhlabs/math/Noise.java
@@ -23,305 +23,273 @@ import java.util.*;
  */
 public class Noise implements Function1D, Function2D, Function3D {
 
-    private static Random randomGenerator = new Random();
+   private static final Random randomGenerator = new Random();
 
-    public float evaluate(float x) {
-        return noise1(x);
-    }
+   public float evaluate(float x) {
+      return noise1(x);
+   }
 
-    public float evaluate(float x, float y) {
-        return noise2(x, y);
-    }
+   public float evaluate(float x, float y) {
+      return noise2(x, y);
+   }
 
-    public float evaluate(float x, float y, float z) {
-        return noise3(x, y, z);
-    }
+   public float evaluate(float x, float y, float z) {
+      return noise3(x, y, z);
+   }
 
-    /**
-     * Compute turbulence using Perlin noise.
-     * @param x the x value
-     * @param y the y value
-     * @param octaves number of octaves of turbulence
-     * @return turbulence value at (x,y)
-     */
-    public static float turbulence2(float x, float y, float octaves) {
-        float t = 0.0f;
+   private final static int B = 0x100;
+   private final static int BM = 0xff;
+   private final static int N = 0x1000;
 
-        for (float f = 1.0f; f <= octaves; f *= 2)
-            t += Math.abs(noise2(f * x, f * y)) / f;
-        return t;
-    }
+   static int[] p = new int[B + B + 2];
+   static float[][] g3 = new float[B + B + 2][3];
+   static float[][] g2 = new float[B + B + 2][2];
+   static float[] g1 = new float[B + B + 2];
+   static boolean start = true;
 
-    /**
-     * Compute turbulence using Perlin noise.
-     * @param x the x value
-     * @param y the y value
-     * @param octaves number of octaves of turbulence
-     * @return turbulence value at (x,y)
-     */
-    public static float turbulence3(float x, float y, float z, float octaves) {
-        float t = 0.0f;
+   private static float sCurve(float t) {
+      return t * t * (3.0f - 2.0f * t);
+   }
 
-        for (float f = 1.0f; f <= octaves; f *= 2)
-            t += Math.abs(noise3(f * x, f * y, f * z)) / f;
-        return t;
-    }
+   /**
+    * Compute 1-dimensional Perlin noise.
+    *
+    * @param x the x value
+    * @return noise value at x in the range -1..1
+    */
+   public static float noise1(float x) {
+      int bx0, bx1;
+      float rx0, rx1, sx, t, u, v;
 
-    private final static int B = 0x100;
-    private final static int BM = 0xff;
-    private final static int N = 0x1000;
+      if (start) {
+         start = false;
+         init();
+      }
 
-    static int[] p = new int[B + B + 2];
-    static float[][] g3 = new float[B + B + 2][3];
-    static float[][] g2 = new float[B + B + 2][2];
-    static float[] g1 = new float[B + B + 2];
-    static boolean start = true;
+      t = x + N;
+      bx0 = ((int) t) & BM;
+      bx1 = (bx0 + 1) & BM;
+      rx0 = t - (int) t;
+      rx1 = rx0 - 1.0f;
 
-    private static float sCurve(float t) {
-        return t * t * (3.0f - 2.0f * t);
-    }
+      sx = sCurve(rx0);
 
-    /**
-     * Compute 1-dimensional Perlin noise.
-     * @param x the x value
-     * @return noise value at x in the range -1..1
-     */
-    public static float noise1(float x) {
-        int bx0, bx1;
-        float rx0, rx1, sx, t, u, v;
+      u = rx0 * g1[p[bx0]];
+      v = rx1 * g1[p[bx1]];
+      return 2.3f * lerp(sx, u, v);
+   }
 
-        if (start) {
-            start = false;
-            init();
-        }
+   /**
+    * Compute 2-dimensional Perlin noise.
+    *
+    * @param x the x coordinate
+    * @param y the y coordinate
+    * @return noise value at (x,y)
+    */
+   public static float noise2(float x, float y) {
+      int bx0, bx1, by0, by1, b00, b10, b01, b11;
+      float rx0;
+      float rx1;
+      float ry0;
+      float ry1;
+      float[] q;
+      float sx;
+      float sy;
+      float a;
+      float b;
+      float t;
+      float u;
+      float v;
+      int i, j;
 
-        t = x + N;
-        bx0 = ((int)t) & BM;
-        bx1 = (bx0+1) & BM;
-        rx0 = t - (int)t;
-        rx1 = rx0 - 1.0f;
+      if (start) {
+         start = false;
+         init();
+      }
 
-        sx = sCurve(rx0);
+      t = x + N;
+      bx0 = ((int) t) & BM;
+      bx1 = (bx0 + 1) & BM;
+      rx0 = t - (int) t;
+      rx1 = rx0 - 1.0f;
 
-        u = rx0 * g1[p[bx0]];
-        v = rx1 * g1[p[bx1]];
-        return 2.3f*lerp(sx, u, v);
-    }
+      t = y + N;
+      by0 = ((int) t) & BM;
+      by1 = (by0 + 1) & BM;
+      ry0 = t - (int) t;
+      ry1 = ry0 - 1.0f;
 
-    /**
-     * Compute 2-dimensional Perlin noise.
-     * @param x the x coordinate
-     * @param y the y coordinate
-     * @return noise value at (x,y)
-     */
-    public static float noise2(float x, float y) {
-        int bx0, bx1, by0, by1, b00, b10, b01, b11;
-        float rx0, rx1, ry0, ry1, q[], sx, sy, a, b, t, u, v;
-        int i, j;
+      i = p[bx0];
+      j = p[bx1];
 
-        if (start) {
-            start = false;
-            init();
-        }
+      b00 = p[i + by0];
+      b10 = p[j + by0];
+      b01 = p[i + by1];
+      b11 = p[j + by1];
 
-        t = x + N;
-        bx0 = ((int)t) & BM;
-        bx1 = (bx0+1) & BM;
-        rx0 = t - (int)t;
-        rx1 = rx0 - 1.0f;
+      sx = sCurve(rx0);
+      sy = sCurve(ry0);
 
-        t = y + N;
-        by0 = ((int)t) & BM;
-        by1 = (by0+1) & BM;
-        ry0 = t - (int)t;
-        ry1 = ry0 - 1.0f;
+      q = g2[b00];
+      u = rx0 * q[0] + ry0 * q[1];
+      q = g2[b10];
+      v = rx1 * q[0] + ry0 * q[1];
+      a = lerp(sx, u, v);
 
-        i = p[bx0];
-        j = p[bx1];
+      q = g2[b01];
+      u = rx0 * q[0] + ry1 * q[1];
+      q = g2[b11];
+      v = rx1 * q[0] + ry1 * q[1];
+      b = lerp(sx, u, v);
 
-        b00 = p[i + by0];
-        b10 = p[j + by0];
-        b01 = p[i + by1];
-        b11 = p[j + by1];
+      return 1.5f * lerp(sy, a, b);
+   }
 
-        sx = sCurve(rx0);
-        sy = sCurve(ry0);
+   /**
+    * Compute 3-dimensional Perlin noise.
+    *
+    * @param x the x coordinate
+    * @param y the y coordinate
+    * @return noise value at (x,y,z)
+    */
+   public static float noise3(float x, float y, float z) {
+      int bx0, bx1, by0, by1, bz0, bz1, b00, b10, b01, b11;
+      float rx0;
+      float rx1;
+      float ry0;
+      float ry1;
+      float rz0;
+      float rz1;
+      float[] q;
+      float sy;
+      float sz;
+      float a;
+      float b;
+      float c;
+      float d;
+      float t;
+      float u;
+      float v;
+      int i, j;
 
-        q = g2[b00]; u = rx0 * q[0] + ry0 * q[1];
-        q = g2[b10]; v = rx1 * q[0] + ry0 * q[1];
-        a = lerp(sx, u, v);
+      if (start) {
+         start = false;
+         init();
+      }
 
-        q = g2[b01]; u = rx0 * q[0] + ry1 * q[1];
-        q = g2[b11]; v = rx1 * q[0] + ry1 * q[1];
-        b = lerp(sx, u, v);
+      t = x + N;
+      bx0 = ((int) t) & BM;
+      bx1 = (bx0 + 1) & BM;
+      rx0 = t - (int) t;
+      rx1 = rx0 - 1.0f;
 
-        return 1.5f*lerp(sy, a, b);
-    }
+      t = y + N;
+      by0 = ((int) t) & BM;
+      by1 = (by0 + 1) & BM;
+      ry0 = t - (int) t;
+      ry1 = ry0 - 1.0f;
 
-    /**
-     * Compute 3-dimensional Perlin noise.
-     * @param x the x coordinate
-     * @param y the y coordinate
-     * @param y the y coordinate
-     * @return noise value at (x,y,z)
-     */
-    public static float noise3(float x, float y, float z) {
-        int bx0, bx1, by0, by1, bz0, bz1, b00, b10, b01, b11;
-        float rx0, rx1, ry0, ry1, rz0, rz1, q[], sy, sz, a, b, c, d, t, u, v;
-        int i, j;
+      t = z + N;
+      bz0 = ((int) t) & BM;
+      bz1 = (bz0 + 1) & BM;
+      rz0 = t - (int) t;
+      rz1 = rz0 - 1.0f;
 
-        if (start) {
-            start = false;
-            init();
-        }
+      i = p[bx0];
+      j = p[bx1];
 
-        t = x + N;
-        bx0 = ((int)t) & BM;
-        bx1 = (bx0+1) & BM;
-        rx0 = t - (int)t;
-        rx1 = rx0 - 1.0f;
+      b00 = p[i + by0];
+      b10 = p[j + by0];
+      b01 = p[i + by1];
+      b11 = p[j + by1];
 
-        t = y + N;
-        by0 = ((int)t) & BM;
-        by1 = (by0+1) & BM;
-        ry0 = t - (int)t;
-        ry1 = ry0 - 1.0f;
+      t = sCurve(rx0);
+      sy = sCurve(ry0);
+      sz = sCurve(rz0);
 
-        t = z + N;
-        bz0 = ((int)t) & BM;
-        bz1 = (bz0+1) & BM;
-        rz0 = t - (int)t;
-        rz1 = rz0 - 1.0f;
+      q = g3[b00 + bz0];
+      u = rx0 * q[0] + ry0 * q[1] + rz0 * q[2];
+      q = g3[b10 + bz0];
+      v = rx1 * q[0] + ry0 * q[1] + rz0 * q[2];
+      a = lerp(t, u, v);
 
-        i = p[bx0];
-        j = p[bx1];
+      q = g3[b01 + bz0];
+      u = rx0 * q[0] + ry1 * q[1] + rz0 * q[2];
+      q = g3[b11 + bz0];
+      v = rx1 * q[0] + ry1 * q[1] + rz0 * q[2];
+      b = lerp(t, u, v);
 
-        b00 = p[i + by0];
-        b10 = p[j + by0];
-        b01 = p[i + by1];
-        b11 = p[j + by1];
+      c = lerp(sy, a, b);
 
-        t  = sCurve(rx0);
-        sy = sCurve(ry0);
-        sz = sCurve(rz0);
+      q = g3[b00 + bz1];
+      u = rx0 * q[0] + ry0 * q[1] + rz1 * q[2];
+      q = g3[b10 + bz1];
+      v = rx1 * q[0] + ry0 * q[1] + rz1 * q[2];
+      a = lerp(t, u, v);
 
-        q = g3[b00 + bz0]; u = rx0 * q[0] + ry0 * q[1] + rz0 * q[2];
-        q = g3[b10 + bz0]; v = rx1 * q[0] + ry0 * q[1] + rz0 * q[2];
-        a = lerp(t, u, v);
+      q = g3[b01 + bz1];
+      u = rx0 * q[0] + ry1 * q[1] + rz1 * q[2];
+      q = g3[b11 + bz1];
+      v = rx1 * q[0] + ry1 * q[1] + rz1 * q[2];
+      b = lerp(t, u, v);
 
-        q = g3[b01 + bz0]; u = rx0 * q[0] + ry1 * q[1] + rz0 * q[2];
-        q = g3[b11 + bz0]; v = rx1 * q[0] + ry1 * q[1] + rz0 * q[2];
-        b = lerp(t, u, v);
+      d = lerp(sy, a, b);
 
-        c = lerp(sy, a, b);
+      return 1.5f * lerp(sz, c, d);
+   }
 
-        q = g3[b00 + bz1]; u = rx0 * q[0] + ry0 * q[1] + rz1 * q[2];
-        q = g3[b10 + bz1]; v = rx1 * q[0] + ry0 * q[1] + rz1 * q[2];
-        a = lerp(t, u, v);
+   public static float lerp(float t, float a, float b) {
+      return a + t * (b - a);
+   }
 
-        q = g3[b01 + bz1]; u = rx0 * q[0] + ry1 * q[1] + rz1 * q[2];
-        q = g3[b11 + bz1]; v = rx1 * q[0] + ry1 * q[1] + rz1 * q[2];
-        b = lerp(t, u, v);
+   private static void normalize2(float[] v) {
+      float s = (float) Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+      v[0] = v[0] / s;
+      v[1] = v[1] / s;
+   }
 
-        d = lerp(sy, a, b);
+   static void normalize3(float[] v) {
+      float s = (float) Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+      v[0] = v[0] / s;
+      v[1] = v[1] / s;
+      v[2] = v[2] / s;
+   }
 
-        return 1.5f*lerp(sz, c, d);
-    }
+   private static int random() {
+      return randomGenerator.nextInt() & 0x7fffffff;
+   }
 
-    public static float lerp(float t, float a, float b) {
-        return a + t * (b - a);
-    }
+   private static void init() {
+      int i, j, k;
 
-    private static void normalize2(float v[]) {
-        float s = (float)Math.sqrt(v[0] * v[0] + v[1] * v[1]);
-        v[0] = v[0] / s;
-        v[1] = v[1] / s;
-    }
+      for (i = 0; i < B; i++) {
+         p[i] = i;
 
-    static void normalize3(float v[]) {
-        float s = (float)Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-        v[0] = v[0] / s;
-        v[1] = v[1] / s;
-        v[2] = v[2] / s;
-    }
+         g1[i] = (float) ((random() % (B + B)) - B) / B;
 
-    private static int random() {
-        return randomGenerator.nextInt() & 0x7fffffff;
-    }
+         for (j = 0; j < 2; j++)
+            g2[i][j] = (float) ((random() % (B + B)) - B) / B;
+         normalize2(g2[i]);
 
-    private static void init() {
-        int i, j, k;
+         for (j = 0; j < 3; j++)
+            g3[i][j] = (float) ((random() % (B + B)) - B) / B;
+         normalize3(g3[i]);
+      }
 
-        for (i = 0; i < B; i++) {
-            p[i] = i;
+      for (i = B - 1; i >= 0; i--) {
+         k = p[i];
+         p[i] = p[j = random() % B];
+         p[j] = k;
+      }
 
-            g1[i] = (float)((random() % (B + B)) - B) / B;
-
-            for (j = 0; j < 2; j++)
-                g2[i][j] = (float)((random() % (B + B)) - B) / B;
-            normalize2(g2[i]);
-
-            for (j = 0; j < 3; j++)
-                g3[i][j] = (float)((random() % (B + B)) - B) / B;
-            normalize3(g3[i]);
-        }
-
-        for (i = B-1; i >= 0; i--) {
-            k = p[i];
-            p[i] = p[j = random() % B];
-            p[j] = k;
-        }
-
-        for (i = 0; i < B + 2; i++) {
-            p[B + i] = p[i];
-            g1[B + i] = g1[i];
-            for (j = 0; j < 2; j++)
-                g2[B + i][j] = g2[i][j];
-            for (j = 0; j < 3; j++)
-                g3[B + i][j] = g3[i][j];
-        }
-    }
-
-    /**
-     * Returns the minimum and maximum of a number of random values
-     * of the given function. This is useful for making some stab at
-     * normalising the function.
-     */
-    public static float[] findRange(Function1D f, float[] minmax) {
-        if (minmax == null)
-            minmax = new float[2];
-        float min = 0, max = 0;
-        // Some random numbers here...
-        for (float x = -100; x < 100; x += 1.27139) {
-            float n = f.evaluate(x);
-            min = Math.min(min, n);
-            max = Math.max(max, n);
-        }
-        minmax[0] = min;
-        minmax[1] = max;
-        return minmax;
-    }
-
-    /**
-     * Returns the minimum and maximum of a number of random values
-     * of the given function. This is useful for making some stab at
-     * normalising the function.
-     */
-    public static float[] findRange(Function2D f, float[] minmax) {
-        if (minmax == null)
-            minmax = new float[2];
-        float min = 0, max = 0;
-        // Some random numbers here...
-        for (float y = -100; y < 100; y += 10.35173) {
-            for (float x = -100; x < 100; x += 10.77139) {
-                float n = f.evaluate(x, y);
-                min = Math.min(min, n);
-                max = Math.max(max, n);
-            }
-        }
-        minmax[0] = min;
-        minmax[1] = max;
-        return minmax;
-    }
+      for (i = 0; i < B + 2; i++) {
+         p[B + i] = p[i];
+         g1[B + i] = g1[i];
+         for (j = 0; j < 2; j++)
+            g2[B + i][j] = g2[i][j];
+         for (j = 0; j < 3; j++)
+            g3[B + i][j] = g3[i][j];
+      }
+   }
 
 }
