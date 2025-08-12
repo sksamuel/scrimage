fd00c661471d0ea20c9566e90f847607cd2ad70e
Enrico Martelli <mrcnrt95e30@gmail.com>
2025-08-12 19:12:18 +0200

    Fix commons-lang3 CVE-2025-48924 (#311)
    This change will update the dependency in order to fix the mentioned CVE

--- scrimage-webp/build.gradle.kts
+++ scrimage-webp/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 
 dependencies {
    api(project(":scrimage-core"))
-   implementation("org.apache.commons:commons-lang3:3.12.0")
+   implementation("org.apache.commons:commons-lang3:3.18.0")
    implementation("org.slf4j:slf4j-api:2.0.6")
 }
