f2935d00efd4f6cce489f6845ded1bb2fc396fc5
sksamuel <sam@sksamuel.com>
2025-09-01 22:43:55 -0500

    Bump commons-io to version 2.19.0 in core and tests modules

--- scrimage-core/build.gradle.kts
+++ scrimage-core/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
    implementation("com.twelvemonkeys.imageio:imageio-core:3.12.0")
    implementation("com.twelvemonkeys.imageio:imageio-jpeg:3.12.0")
    implementation("com.drewnoakes:metadata-extractor:2.19.0")
-   implementation("commons-io:commons-io:2.14.0")
+   implementation("commons-io:commons-io:2.19.0")
    implementation("ar.com.hjg:pngj:2.1.0")
 }
--- scrimage-tests/build.gradle.kts
+++ scrimage-tests/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
    implementation("com.twelvemonkeys.imageio:imageio-core:3.12.0")
    implementation("com.twelvemonkeys.imageio:imageio-jpeg:3.12.0")
    implementation("com.drewnoakes:metadata-extractor:2.19.0")
-   implementation("commons-io:commons-io:2.11.0")
+   implementation("commons-io:commons-io:2.19.0")
 }
