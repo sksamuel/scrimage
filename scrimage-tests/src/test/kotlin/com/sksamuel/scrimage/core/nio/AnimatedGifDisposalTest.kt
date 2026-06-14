sksamuel/scrimage

commit aefe087b2d15c1cc0bcc6875f8b0176df6765c72

Add dispose method support when writing frames with StreamingGifWriter (#242)
* Added dispose method functionality - adjusted GifReader to properly respect disposeMethod according to spec - tests

* Consolidate GraphicsControlExtension code

Co-authored-by: zach.almberg <zach.almberg@jamf.com>

scrimage-core/src/main/java/com/sksamuel/scrimage/nio/internal/GifSequenceReader.java
@@ -6,6 +6,8 @@ import java.util.*;
 import java.awt.*;
 import java.awt.image.*;
 
+import com.sksamuel.scrimage.DisposeMethod;
+
 /**
  * Class GifDecoder - Decodes a GIF file into one or more frames.
  *
@@ -78,10 +80,10 @@ public class GifSequenceReader {
    protected byte[] block = new byte[256]; // current data block
    protected int blockSize = 0; // block size
 
+   protected int frameIndexWithLastDoNotDispose = 0;
    // last graphic control extension info
-   protected int dispose = 0;
    // 0=no action; 1=leave in place; 2=restore to bg; 3=restore to prev
-   protected int lastDispose = 0;
+   protected int dispose = 0;
    protected boolean transparency = false; // use transparent color
    protected int delay = 0; // delay in milliseconds
    protected int transIndex; // transparent color index
@@ -99,12 +101,14 @@ public class GifSequenceReader {
    protected int frameCount;
 
    static class GifFrame {
-      public GifFrame(BufferedImage im, int del) {
+      public GifFrame(BufferedImage im, int del, int disposeMethod) {
          image = im;
          delay = del;
+         this.disposeMethod = disposeMethod;
       }
       public BufferedImage image;
       public int delay;
+      public int disposeMethod;
    }
 
    /**
@@ -120,6 +124,21 @@ public class GifSequenceReader {
          delay = ((GifFrame) frames.get(n)).delay;
       }
       return delay;
+   }
+
+   /**
+    * Gets dispose method for specified frame.
+    *
+    * @param n int index of frame
+    * @return DisposeMethod enum for given frame
+    */
+   public DisposeMethod getDisposeMethod(int n) {
+      //
+      dispose = 0;
+      if ((n >= 0) && (n < frameCount)) {
+         dispose = ((GifFrame) frames.get(n)).disposeMethod;
+      }
+      return DisposeMethod.getDisposeMethodFromId(dispose);
    }
 
    /**
@@ -158,13 +177,12 @@ public class GifSequenceReader {
       int[] dest =
          ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
-      // fill in starting image contents based on last image's dispose code
-      if (lastDispose > 0) {
-         if (lastDispose == 3) {
-            // use image before last
-            int n = frameCount - 2;
-            if (n > 0) {
-               lastImage = getFrame(n - 1);
+      // fill in starting image contents based on the current image's dispose code
+      if (dispose > 0) {
+         if (dispose == 3) {
+            // use the last image with a 'doNotDispose' code
+            if (frameIndexWithLastDoNotDispose > 0) {
+               lastImage = getFrame(frameIndexWithLastDoNotDispose);
             } else {
                lastImage = null;
             }
@@ -176,7 +194,7 @@ public class GifSequenceReader {
             System.arraycopy(prev, 0, dest, 0, width * height);
             // copy pixels
 
-            if (lastDispose == 2) {
+            if (dispose == 2) {
                // fill last image rect area with background color
                Graphics2D g = image.createGraphics();
                Color c = null;
@@ -700,11 +718,11 @@ public class GifSequenceReader {
 
       // create new image to receive frame data
       image =
-         new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB_PRE);
+         new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
       setPixels(); // transfer pixel data to image
 
-      frames.add(new GifFrame(image, delay)); // add image to frame list
+      frames.add(new GifFrame(image, delay, dispose)); // add image to frame list
 
       if (transparency) {
          act[transIndex] = save;
@@ -760,7 +778,9 @@ public class GifSequenceReader {
     * Resets frame state for reading next image.
     */
    protected void resetFrame() {
-      lastDispose = dispose;
+      if (dispose == 1) {
+         this.frameIndexWithLastDoNotDispose = frameCount-1;
+      }
       lastRect = new Rectangle(ix, iy, iw, ih);
       lastImage = image;
       lastBgColor = bgColor;

scrimage-core/src/main/java/com/sksamuel/scrimage/DisposeMethod.java
@@ -1,0 +1,54 @@
+package com.sksamuel.scrimage;
+
+import java.util.Arrays;
+
+/**
+ * When gifs transition from frame to frame, the new frame is overlaid on top of the previous frame, with the
+ * 'disposeMethod' metadata property of each frame telling the current frame what to do with the previous frame
+ * once the new frame is displayed.
+ */
+public enum DisposeMethod {
+
+   /**
+    * 'none' or unspecified replaces the entire non-transparent frame with the new one.
+    */
+   NONE(0, "none"),
+
+   /**
+    * 'doNotDispose' tells the current frame to display the pixels from the previous frame that
+    * were not replaced with the new frame. When not dealing with transparent frames 'none' and
+    * 'doNotDispose' behave the same.
+    */
+   DO_NOT_DISPOSE(1, "doNotDispose"),
+
+   /**
+    * 'restoreToBackgroundColor' makes the background color or background tile show through the transparent
+    * pixels of the new frame (replacing the image areas of the previous frame).
+    */
+   RESTORE_TO_BACKGROUND_COLOR(2, "restoreToBackgroundColor"),
+
+   /**
+    * 'restoreToPrevious' restores to the state of the most recent un-disposed frame. For example, if you
+    * have a static background that is set to Do Not Dispose, that image will reappear in the areas left
+    * by a replaced frame (until a new frame has been declared as 'doNotDispose').
+    */
+   RESTORE_TO_PREVIOUS(3, "restoreToPrevious");
+
+   private final int id;
+   private final String disposeMethodValue;
+
+   DisposeMethod(int id, String disposeMethodValue) {
+      this.id = id;
+      this.disposeMethodValue = disposeMethodValue;
+   }
+
+   public String getDisposeMethodValue() {
+      return disposeMethodValue;
+   }
+
+   public static DisposeMethod getDisposeMethodFromId(int id) {
+      return Arrays.stream(DisposeMethod.values())
+         .filter(disposeMethod -> disposeMethod.id == id)
+         .findFirst().orElseThrow(() -> new RuntimeException("DisposeMethod with id [" + id + "] is invalid"));
+   }
+}

scrimage-core/src/main/java/com/sksamuel/scrimage/nio/AnimatedGif.java
@@ -1,5 +1,6 @@
 package com.sksamuel.scrimage.nio;
 
+import com.sksamuel.scrimage.DisposeMethod;
 import com.sksamuel.scrimage.ImmutableImage;
 import com.sksamuel.scrimage.nio.internal.GifSequenceReader;
 
@@ -30,6 +31,10 @@ public class AnimatedGif {
 
    public Duration getDelay(int frame) {
       return Duration.ofMillis(reader.getDelay(frame));
+   }
+
+   public DisposeMethod getDisposeMethod(int frame) {
+      return reader.getDisposeMethod(frame);
    }
 
    public ImmutableImage getFrame(int n) {

scrimage-core/src/main/java/com/sksamuel/scrimage/nio/StreamingGifWriter.java
@@ -1,5 +1,6 @@
 package com.sksamuel.scrimage.nio;
 
+import com.sksamuel.scrimage.DisposeMethod;
 import com.sksamuel.scrimage.ImmutableImage;
 
 import javax.imageio.IIOImage;
@@ -7,6 +8,7 @@ import javax.imageio.ImageIO;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOInvalidTreeException;
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.stream.MemoryCacheImageOutputStream;
@@ -65,6 +67,10 @@ public class StreamingGifWriter extends AbstractGifWriter {
       GifStream writeFrame(ImmutableImage image) throws IOException;
 
       GifStream writeFrame(ImmutableImage image, Duration delay) throws IOException;
+
+      GifStream writeFrame(ImmutableImage image, DisposeMethod disposeMethod) throws IOException;
+
+      GifStream writeFrame(ImmutableImage image, Duration delay, DisposeMethod disposalMethod) throws IOException;
    }
 
    public GifStream prepareStream(String path, int imageType) throws IOException {
@@ -111,15 +117,32 @@ public class StreamingGifWriter extends AbstractGifWriter {
 
          @Override
          public GifStream writeFrame(ImmutableImage image, Duration delay) throws IOException {
+            return writeFrame(image, delay, null);
+         }
+
+         @Override
+         public GifStream writeFrame(ImmutableImage image, DisposeMethod disposeMethod) throws IOException {
+            return writeFrame(image, null, disposeMethod);
+         }
+
+         @Override
+         public GifStream writeFrame(ImmutableImage image, Duration delay, DisposeMethod disposalMethod) throws IOException {
+            setOverriddenGraphicControlExtensionAttributes(delay, disposalMethod);
+            writer.writeToSequence(new IIOImage(image.awt(), null, imageMetaData), imageWriteParam);
+            return this;
+         }
+
+         private void setOverriddenGraphicControlExtensionAttributes(Duration delay, DisposeMethod disposeMethod)
+            throws IIOInvalidTreeException {
             IIOMetadataNode rootOverride = (IIOMetadataNode) imageMetaData.getAsTree(metaFormatName);
             IIOMetadataNode graphicsControlExtensionNodeOverride = getNode(rootOverride, "GraphicControlExtension");
-            graphicsControlExtensionNodeOverride.setAttribute("delayTime", (delay.toMillis() / 10L) + "");
+            if (delay != null) {
+               graphicsControlExtensionNodeOverride.setAttribute("delayTime", (delay.toMillis() / 10L) + "");
+            }
+            if (disposeMethod != null) {
+               graphicsControlExtensionNodeOverride.setAttribute("disposalMethod", disposeMethod.getDisposeMethodValue());
+            }
             imageMetaData.setFromTree(metaFormatName, rootOverride);
-
-            writer.writeToSequence(new IIOImage(image.awt(), null, imageMetaData), imageWriteParam);
-
-            imageMetaData.setFromTree(metaFormatName, root);
-            return this;
          }
 
          @Override

scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/DisposeMethodTest.kt
@@ -1,0 +1,29 @@
+package com.sksamuel.scrimage.core
+
+import com.sksamuel.scrimage.DisposeMethod
+import io.kotest.core.spec.style.FunSpec
+import org.junit.jupiter.api.assertThrows
+
+class DisposeMethodTest : FunSpec({
+
+   test("Test getDisposeMethodFromId") {
+      assertThrows<RuntimeException>("DisposeMethod with id [-1] is invalid") {
+         DisposeMethod.getDisposeMethodFromId(-1)
+      }
+      assert(DisposeMethod.getDisposeMethodFromId(0) == DisposeMethod.NONE)
+      assert(DisposeMethod.getDisposeMethodFromId(1) == DisposeMethod.DO_NOT_DISPOSE)
+      assert(DisposeMethod.getDisposeMethodFromId(2) == DisposeMethod.RESTORE_TO_BACKGROUND_COLOR)
+      assert(DisposeMethod.getDisposeMethodFromId(3) == DisposeMethod.RESTORE_TO_PREVIOUS)
+      assertThrows<RuntimeException>("DisposeMethod with id [4] is invalid") {
+         DisposeMethod.getDisposeMethodFromId(4)
+      }
+   }
+
+   test("Test strings match spec") {
+      assert(DisposeMethod.NONE.disposeMethodValue == "none")
+      assert(DisposeMethod.DO_NOT_DISPOSE.disposeMethodValue == "doNotDispose")
+      assert(DisposeMethod.RESTORE_TO_BACKGROUND_COLOR.disposeMethodValue == "restoreToBackgroundColor")
+      assert(DisposeMethod.RESTORE_TO_PREVIOUS.disposeMethodValue == "restoreToPrevious")
+   }
+
+})

scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/nio/StreamingGifWriterTest.kt
@@ -3,8 +3,11 @@
 package com.sksamuel.scrimage.core.nio
 
 import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.AnimatedGifReader
+import com.sksamuel.scrimage.nio.ImageSource
 import com.sksamuel.scrimage.nio.StreamingGifWriter
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.wordSpec
 import io.kotest.matchers.shouldBe
 import org.apache.commons.io.IOUtils
 import java.awt.image.BufferedImage
@@ -12,9 +15,12 @@ import java.io.ByteArrayOutputStream
 import java.time.Duration
 
 class StreamingGifWriterTest : WordSpec({
+   // run dispose method tests
+   include(disposeMethodTests( "/com/sksamuel/scrimage/nio/animated_birds.gif")) // tests 'doNotDispose'
+   include(disposeMethodTests( "/com/sksamuel/scrimage/nio/bananaDance.gif")) // tests 'restoreToBackgroundColor'
+   include(disposeMethodTests( "/com/sksamuel/scrimage/nio/canvas_prev.gif")) // tests 'restoreToPrevious'
 
    val bird = ImmutableImage.loader().fromResource("/bird_small.png")
-
    "StreamingGifWriter" should {
       "write to memory"  {
          val writer = StreamingGifWriter().withFrameDelay(Duration.ofMillis(500))
@@ -32,3 +38,32 @@ class StreamingGifWriterTest : WordSpec({
    }
 
 })
+
+fun disposeMethodTests(resourcePath: String) = wordSpec {
+   val originalGif = AnimatedGifReader.read(ImageSource.of(javaClass.getResourceAsStream(resourcePath)))
+
+   "StreamingGifWriter" should {
+      val writer = StreamingGifWriter()
+      val output = ByteArrayOutputStream()
+      val stream = writer.prepareStream(output, BufferedImage.TYPE_INT_ARGB)
+
+      for ((i, frame) in originalGif.frames.withIndex()) {
+         "write frame with index \"$i\" to stream with \"${originalGif.getDisposeMethod(i)}\" dispose method" {
+            stream.writeFrame(frame, originalGif.getDelay(i), originalGif.getDisposeMethod(i))
+         }
+      }
+
+      "close" {
+         stream.close()
+      }
+
+      "contents from $resourcePath and output should be equal" {
+         val outputGif = AnimatedGifReader.read(ImageSource.of(output.toByteArray()))
+         for((i, frame) in outputGif.frames.withIndex()) {
+            outputGif.getDisposeMethod(i) shouldBe originalGif.getDisposeMethod(i)
+            outputGif.getDelay(i) shouldBe originalGif.getDelay(i)
+            frame.pixels() shouldBe originalGif.getFrame(i).pixels()
+         }
+      }
+   }
+}

commit 2f2c44f08112883f1d4ee046baaa3be293692230

Apply the previous frame's disposal method when decoding animated GIFs
Per the GIF89a spec a frame's disposal method describes what to do with the
canvas after that frame is shown, before the next frame is composited. So when
building frame N's canvas the decoder must consult frame N-1's disposal method.
GifSequenceReader.setPixels used the *current* frame's `dispose` field instead -
the tell is that lastImage, lastRect and lastBgColor in that block are all
previous-frame values, with `dispose` the lone current-frame value.

This regressed in #242 (commit aefe087b), which collapsed the original Kevin
Weiner `lastDispose` field into `dispose`. Restore a `lastDispose` field, capture
it in resetFrame, and key the disposal block off it.

Also fix the restore-to-previous base-frame lookup: frameIndexWithLastDoNotDispose
was initialised to 0 and tested with `> 0`, conflating "no do-not-dispose frame
seen" with "frame 0 is the base". Initialise to -1 and test `>= 0` so a GIF whose
do-not-dispose base is frame 0 (the common static-background case) restores to it
instead of to a blank/transparent canvas.

These only diverge when consecutive frames use different disposal methods, which
is why the all-Background (bananaDance) and all-Previous (canvas_prev) round-trip
fixtures never caught it - and why the existing reader->writer->reader tests,
comparing the reader against itself, couldn't either.

Regression tests use an independent oracle (ImageMagick -coalesce):
- mixed_disposal.gif (restore-to-background then none): the disposed frame's
  pixels are cleared, not bled into the next frame.
- canvas_prev.gif (restore-to-previous onto a frame-0 background): the background
  survives outside the patch instead of vanishing.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

scrimage-core/src/main/java/com/sksamuel/scrimage/nio/internal/GifSequenceReader.java
@@ -90,10 +90,17 @@ public class GifSequenceReader {
    protected byte[] block = new byte[256]; // current data block
    protected int blockSize = 0; // block size
 
-   protected int frameIndexWithLastDoNotDispose = 0;
+   // -1 means "no do-not-dispose frame seen yet"; distinguishing this from a
+   // legitimate index 0 matters for restore-to-previous (see setPixels).
+   protected int frameIndexWithLastDoNotDispose = -1;
    // last graphic control extension info
    // 0=no action; 1=leave in place; 2=restore to bg; 3=restore to prev
    protected int dispose = 0;
+   // The disposal code of the PREVIOUS frame. Per the GIF spec a frame's disposal
+   // method describes what to do with the canvas after that frame is shown, before
+   // the next frame is composited - so building frame N's canvas must consult frame
+   // N-1's disposal, not frame N's.
+   protected int lastDispose = 0;
    protected boolean transparency = false; // use transparent color
    protected int delay = 0; // delay in milliseconds
    protected int transIndex; // transparent color index
@@ -187,11 +194,11 @@ public class GifSequenceReader {
       int[] dest =
          ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
-      // fill in starting image contents based on the current image's dispose code
-      if (dispose > 0) {
-         if (dispose == 3) {
+      // fill in starting image contents based on the PREVIOUS frame's dispose code
+      if (lastDispose > 0) {
+         if (lastDispose == 3) {
             // use the last image with a 'doNotDispose' code
-            if (frameIndexWithLastDoNotDispose > 0) {
+            if (frameIndexWithLastDoNotDispose >= 0) {
                lastImage = getFrame(frameIndexWithLastDoNotDispose);
             } else {
                lastImage = null;
@@ -204,7 +211,7 @@ public class GifSequenceReader {
             System.arraycopy(prev, 0, dest, 0, width * height);
             // copy pixels
 
-            if (dispose == 2) {
+            if (lastDispose == 2) {
                // fill last image rect area with background color
                Graphics2D g = image.createGraphics();
                try {
@@ -825,6 +832,8 @@ public class GifSequenceReader {
       lastRect = new Rectangle(ix, iy, iw, ih);
       lastImage = image;
       lastBgColor = bgColor;
+      // Remember this frame's disposal so the next frame's setPixels can act on it.
+      lastDispose = dispose;
       // These three were previously declared as locals, shadowing the
       // instance fields. The intent is to reset the frame state ready for
       // the next image: per the GIF spec, the graphics control extension

scrimage-tests/src/test/kotlin/com/sksamuel/scrimage/core/nio/AnimatedGifDisposalTest.kt
@@ -1,0 +1,51 @@
+package com.sksamuel.scrimage.core.nio
+
+import com.sksamuel.scrimage.nio.AnimatedGifReader
+import com.sksamuel.scrimage.nio.ImageSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+/**
+ * Regression tests for frame disposal handling in the GIF reader. When building frame N's
+ * canvas the reader must apply frame N-1's disposal method (the GIF spec defines disposal as
+ * what to do with the canvas after a frame is shown, before the next is composited). The
+ * reader previously consulted frame N's own disposal, which only matters when consecutive
+ * frames use different disposal methods - so neither existing all-Background nor all-Previous
+ * fixture exposed it. Expected pixels below were cross-checked against ImageMagick `-coalesce`.
+ */
+class AnimatedGifDisposalTest : FunSpec({
+
+   test("restore-to-background disposal of the previous frame clears its pixels") {
+      // mixed_disposal.gif: frame 0 = red 5x5 at top-left, dispose=restoreToBackground;
+      // frame 1 = blue 5x5 at bottom-right, dispose=none. Frame 0 must be cleared before
+      // frame 1, so frame 1's top-left is transparent background, not frame 0's red.
+      val gif = AnimatedGifReader.read(
+         ImageSource.of(javaClass.getResourceAsStream("/com/sksamuel/scrimage/nio/mixed_disposal.gif"))
+      )
+      gif.frameCount shouldBe 2
+      val frame1 = gif.getFrame(1)
+      // top-left was red in frame 0; after restore-to-background it must be transparent
+      frame1.pixel(0, 0).alpha() shouldBe 0
+      // the blue patch frame 1 actually carries is present
+      frame1.pixel(7, 7).toARGBInt() shouldBe java.awt.Color(0, 0, 255).rgb
+   }
+
+   test("restore-to-previous disposal restores to a frame-0 base image") {
+      // canvas_prev.gif: frame 0 = full 100x100 skyblue background (doNotDispose); frames 1-4
+      // are 32x32 patches with dispose=restoreToPrevious. Each later frame must be composited
+      // onto the restored frame-0 background, so background pixels outside the patch survive.
+      val gif = AnimatedGifReader.read(
+         ImageSource.of(javaClass.getResourceAsStream("/com/sksamuel/scrimage/nio/canvas_prev.gif"))
+      )
+      val frame0 = gif.getFrame(0)
+      val frame1 = gif.getFrame(1)
+      val bg = frame0.pixel(0, 0)
+      // sanity: frame 0's corner is an opaque background colour, not transparent
+      bg.alpha() shouldBe 255
+      // frame 1's corner (outside any patch) must still show the frame-0 background,
+      // not transparent (which is what the lost-base-frame bug produced).
+      frame1.pixel(0, 0).toARGBInt() shouldBe bg.toARGBInt()
+      frame1.pixel(99, 99).alpha() shouldNotBe 0
+   }
+})